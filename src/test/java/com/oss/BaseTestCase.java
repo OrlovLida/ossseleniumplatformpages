255ef1bc3cd025b0dedce666898d044781cdd739
Piotr.Kubik <piotr.kubik@comarch.com>
2020-06-12 14:43:37 +0200

    change Base_URL to system property

--- src/test/java/com/oss/BaseTestCase.java
+++ src/test/java/com/oss/BaseTestCase.java
@@ -14,7 +14,8 @@ import static com.oss.configuration.Configuration.CONFIGURATION;
 
 public class BaseTestCase {
 
-    public static final String BASIC_URL = CONFIGURATION.getValue("baseUrl");
+    public static final String BASIC_URL = System.getProperty("URL");
+   // public static final String BASIC_URL = CONFIGURATION.getValue("baseUrl");
     public static final String MOCK_PATH = CONFIGURATION.getValue("mockPath");
 
     public WebDriver driver;
