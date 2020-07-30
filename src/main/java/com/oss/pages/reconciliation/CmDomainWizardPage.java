047e2371350438dacc83b1f361e914f8423b1723
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2020-07-30 14:02:31 +0200

    ER OSSWEB-7879 # add seleniumTests - reconciliation test - adding Steps description

--- src/main/java/com/oss/pages/reconciliation/CmDomainWizardPage.java
+++ src/main/java/com/oss/pages/reconciliation/CmDomainWizardPage.java
@@ -8,6 +8,8 @@ import com.oss.framework.utils.DelayUtils;
 import com.oss.framework.widgets.Wizard;
 import com.oss.pages.BasePage;
 
+import io.qameta.allure.Step;
+
 public class CmDomainWizardPage extends BasePage {
     private Wizard wizard;
 
@@ -22,6 +24,7 @@ public class CmDomainWizardPage extends BasePage {
         return wizard;
     }
 
+    @Step("Fill CM Domain Wizard fields and Save CM Domain")
     public void fillCmDomainWizardAndClose(String cmDomainName, String cmInterfaceName, String domainName) {
         Wizard wizard = Wizard.createWizard(driver, wait);
         Input name = wizard.getComponent("narComponent_networkDiscoveryControlViewIdnameTextFieldId", ComponentType.TEXT_FIELD);
