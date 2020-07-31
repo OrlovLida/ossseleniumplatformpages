c889e4d392c20d4b6cc7f2480a97e13bef6ca035
Piotr.Kubik <piotr.kubik@comarch.com>
2020-07-31 10:32:22 +0000

    Resolve OSSWEB-6854

--- src/main/java/com/oss/pages/exportguiwizard/FillServerDataPage.java
+++ src/main/java/com/oss/pages/exportguiwizard/FillServerDataPage.java
@@ -1,6 +1,7 @@
 package com.oss.pages.exportguiwizard;
 
 import com.oss.framework.data.Data;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
 public class FillServerDataPage extends ExportGuiWizardPage{
@@ -13,26 +14,31 @@ public class FillServerDataPage extends ExportGuiWizardPage{
     private String USER_NAME_ID = "exportgui-components-remoteusernametext";
     private String PASSWORD_ID = "exportgui-components-remotepasswordtext";
 
+    @Step("Type Server Address")
     public FillServerDataPage typeServerAddress(String serverAddress) {
         setValueOnTextField(SERVER_ADDRESS_ID,Data.createSingleData(serverAddress));
         return this;
     }
 
+    @Step("Type Remote Directory Path")
     public FillServerDataPage typeRemoteDirectoryPath(String remoteDirectoryPath) {
         setValueOnTextField(REMOTE_DIRECTORY_PATH_ID,Data.createSingleData(remoteDirectoryPath));
         return this;
     }
 
+    @Step("Type User Name")
     public FillServerDataPage typeUserName(String userName) {
         setValueOnTextField(USER_NAME_ID,Data.createSingleData(userName));
         return this;
     }
 
+    @Step("Type password")
     public FillServerDataPage typePassword(String password) {
         setValueOnTextField(PASSWORD_ID,Data.createSingleData(password));
         return this;
     }
 
+    @Step("Choose Protocole Type")
     public FillServerDataPage chooseProtocoleType(String type){
         waitForComponent("//input[contains (@id,'" + PROTOCOLE_TYPE_ID + "')]");
         setValueOnCombobox(PROTOCOLE_TYPE_ID, type);
