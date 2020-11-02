d2f9d79e00ac2b74558dcb11c19eebe28c550308
Bartlomiej.Stec <bartlomiej.stec@comarch.com>
2020-11-02 11:33:11 +0000

    ER OSSTEST-97583 # adds HostingRelationCreationTest

--- src/test/java/com/oss/radio/HostingRelationCreationTest.java
+++ src/test/java/com/oss/radio/HostingRelationCreationTest.java
@@ -1,28 +1,49 @@
 package com.oss.radio;
 
 import com.oss.BaseTestCase;
+import com.oss.framework.mainheader.PerspectiveChooser;
 import com.oss.framework.utils.DelayUtils;
 import com.oss.pages.radio.CellSiteConfigurationPage;
 import io.qameta.allure.Description;
-import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import static java.lang.String.format;
 
 public class HostingRelationCreationTest extends BaseTestCase {
     private CellSiteConfigurationPage cellSiteConfigurationPage;
+    private String eNodeBName = "SeleniumENodeBTest";
+    private String locationName = "SeleniumLocationTest";
+    private String hostingTabName = "Hosting";
 
     public static void goToCellSiteConfiguration(WebDriver driver, String basicURL) {
         DelayUtils.sleep(1000);
-        driver.get(format("#/view/radio/cellsite/xid?ids=62260378&perspective=LIVE", basicURL));
+        driver.get(format("%s#/view/radio/cellsite/xid?ids=75521556&perspective=LIVE", basicURL));
     }
 
     @BeforeClass
     @Description("Open Cell Site Configuration View")
     public void openCellSiteConfigurationView() {
+        DelayUtils.waitForPageToLoad(driver, webDriverWait);
+        PerspectiveChooser.create(driver, webDriverWait).setLivePerspective();
         goToCellSiteConfiguration(driver, BASIC_URL);
         cellSiteConfigurationPage = new CellSiteConfigurationPage(driver);
+    }
+
+    @Test(priority = 1)
+    @Description("Select eNodeB from tree")
+    public void selectBaseStation() {
+        DelayUtils.waitForPageToLoad(driver, webDriverWait);
+        cellSiteConfigurationPage.expandTreeToBaseStation("Site", locationName, eNodeBName);
+        DelayUtils.waitForPageToLoad(driver, webDriverWait);
+    }
+
+    @Test(priority = 2)
+    @Description("Go to Hosting tab and create hosting relation")
+    public void createHostingRelation() {
+        cellSiteConfigurationPage.selectTab(hostingTabName);
+        cellSiteConfigurationPage.clickPlusIconByLabel("Host on Device");
     }
 
 
