31501738e7d03d545fe685ceb8ae12a592999acb
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2020-12-01 11:01:50 +0100

    ER OSSTEST-98386 # add more inputs to CableWizardPage # handle assignLocation for RAN Devices # add E2E scenario

--- src/main/java/com/oss/pages/radio/CableWizardPage.java
+++ src/main/java/com/oss/pages/radio/CableWizardPage.java
@@ -9,6 +9,7 @@ import com.oss.pages.BasePage;
 import io.qameta.allure.Step;
 
 import static com.oss.framework.components.inputs.Input.ComponentType.SEARCH_FIELD;
+import static com.oss.framework.components.inputs.Input.ComponentType.TEXT_FIELD;
 
 public class CableWizardPage extends BasePage {
 
@@ -22,5 +23,34 @@ public class CableWizardPage extends BasePage {
     public void setModel(String model) {
         Input input = wizard.getComponent("model", SEARCH_FIELD);
         input.setSingleStringValue(model);
+    }
+
+    @Step("Set name")
+    public void setName(String name) {
+        Input input = wizard.getComponent("name", TEXT_FIELD);
+        input.setSingleStringValue(name);
+    }
+
+    @Step("Set start device")
+    public void setStartDevice(String device) {
+        Input input = wizard.getComponent("startDeviceTermination", SEARCH_FIELD);
+        input.setSingleStringValue(device);
+    }
+
+    @Step("Set end device")
+    public void setEndDevice(String device) {
+        Input input = wizard.getComponent("endDeviceTermination", SEARCH_FIELD);
+        input.setSingleStringValue(device);
+    }
+
+    @Step("Set end location")
+    public void setEndLocation(String device) {
+        Input input = wizard.getComponent("endTermination", SEARCH_FIELD);
+        input.setSingleStringValue(device);
+    }
+
+    @Step("Click accept")
+    public void accept() {
+        wizard.clickAccept();
     }
 }
