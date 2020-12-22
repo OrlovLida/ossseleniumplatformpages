6afb85de9354a52bc2b733b7e57e2361074474ec
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2020-12-22 16:54:17 +0100

    ER OSSTEST-99779 # page corrections

--- src/main/java/com/oss/pages/transport/IRBInterfaceWizardPage.java
+++ src/main/java/com/oss/pages/transport/IRBInterfaceWizardPage.java
@@ -20,24 +20,17 @@ public class IRBInterfaceWizardPage extends BasePage {
     private static final String IRB_DESCRIPTION_ID = "irb-main-description";
     private static final String WIZARD_ID = "Popup";
 
-    public static IRBInterfaceWizardPage goToIRBInterfaceWizardPage(WebDriver driver, String basicURL) {
-        driver.get(String.format("%s/#/view/transport/ip/ethernet/irb-interface?" + "perspective=LIVE", basicURL));
-        return new IRBInterfaceWizardPage(driver);
-    }
-
     public IRBInterfaceWizardPage(WebDriver driver) {
         super(driver);
     }
 
-    private Wizard wizard = Wizard.createByComponentId(driver, wait, WIZARD_ID);
-
     @Step("Create IRB Interface with device name {device} and VlanID {vlanId}")
     public IRBInterfaceWizardPage createIRBInterface(String device, String vlanId) {
-        wizard.setComponentValue(IRB_DEVICE_ID, device, Input.ComponentType.SEARCH_FIELD);
+        getWizard().setComponentValue(IRB_DEVICE_ID, device, Input.ComponentType.SEARCH_FIELD);
         waitForPageToLoad();
-        wizard.setComponentValue(IRB_VLANID_ID, vlanId, Input.ComponentType.TEXT_FIELD);
+        getWizard().setComponentValue(IRB_VLANID_ID, vlanId, Input.ComponentType.TEXT_FIELD);
         waitForPageToLoad();
-        wizard.clickAccept();
+        getWizard().clickAccept();
         return this;
     }
 
@@ -48,24 +41,28 @@ public class IRBInterfaceWizardPage extends BasePage {
         newInventoryViewPage.editTextFields(IPADDRESS_ID, Input.ComponentType.TEXT_FIELD, ipAddress);
         newInventoryViewPage.editTextFields(IPSUBNET_ID, Input.ComponentType.SEARCH_FIELD, subnet);
         waitForPageToLoad();
-        wizard.clickButtonByLabel("Next Step");
+        getWizard().clickButtonByLabel("Next Step");
         waitForPageToLoad();
-        wizard.clickAccept();
+        getWizard().clickAccept();
         return this;
     }
 
     @Step("Edit IRB Interface and set MTU {mtu} and description {description}")
     public IRBInterfaceWizardPage editIRBInterface(String mtu, String description) {
         waitForPageToLoad();
-        wizard.setComponentValue(IRB_MTU_ID, mtu, Input.ComponentType.TEXT_FIELD);
+        getWizard().setComponentValue(IRB_MTU_ID, mtu, Input.ComponentType.TEXT_FIELD);
         waitForPageToLoad();
-        wizard.setComponentValue(IRB_DESCRIPTION_ID, description, Input.ComponentType.TEXT_FIELD);
+        getWizard().setComponentValue(IRB_DESCRIPTION_ID, description, Input.ComponentType.TEXT_FIELD);
         waitForPageToLoad();
-        wizard.clickAccept();
+        getWizard().clickAccept();
         return this;
     }
 
     private void waitForPageToLoad() {
         DelayUtils.waitForPageToLoad(driver, wait);
+    }
+
+    private Wizard getWizard() {
+        return Wizard.createByComponentId(driver, wait, WIZARD_ID);
     }
 }
