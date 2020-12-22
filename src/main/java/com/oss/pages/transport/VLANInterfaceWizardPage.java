d0ed5cc9c8db8d2facc16c23c5a522e7d0fe5d28
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2020-12-22 18:10:28 +0100

    ER OSSTEST-99780 # VLAN Interface tests

--- src/main/java/com/oss/pages/transport/VLANInterfaceWizardPage.java
+++ src/main/java/com/oss/pages/transport/VLANInterfaceWizardPage.java
@@ -3,10 +3,14 @@ package com.oss.pages.transport;
 import org.openqa.selenium.WebDriver;
 
 import com.oss.framework.components.inputs.Input.ComponentType;
+import com.oss.framework.utils.DelayUtils;
 import com.oss.framework.widgets.Wizard;
 import com.oss.pages.BasePage;
 
 import io.qameta.allure.Step;
+
+import static com.oss.framework.components.inputs.Input.ComponentType.NUMBER_FIELD;
+import static com.oss.framework.components.inputs.Input.ComponentType.TEXT_FIELD;
 
 public class VLANInterfaceWizardPage extends BasePage {
 
@@ -19,6 +23,8 @@ public class VLANInterfaceWizardPage extends BasePage {
     private static final String WIZARD_ID = "vlanInterfaceWizard";
     private static final String ACCEPT_ID = "vlanInterfaceWizardApp-finish";
     private static final String NEXT_ID = "vlanInterfaceWizardApp-next";
+    private static final String IRB_MTU_ID = "mtu-uid";
+    private static final String IRB_DESCRIPTION_ID = "description-uid";
 
     @Step("Set VLAN Interface type to {type}")
     public void setType(String type) {
@@ -27,7 +33,7 @@ public class VLANInterfaceWizardPage extends BasePage {
 
     @Step("Set Subinterface ID to {subinterfaceId}")
     public void setSubinterfaceId(String subinterfaceId) {
-        getWizard().setComponentValue(SUBINTERFACE_ID, subinterfaceId, ComponentType.NUMBER_FIELD);
+        getWizard().setComponentValue(SUBINTERFACE_ID, subinterfaceId, NUMBER_FIELD);
     }
 
     //TODO correct
@@ -46,7 +52,21 @@ public class VLANInterfaceWizardPage extends BasePage {
         getWizard().clickActionById(ACCEPT_ID);
     }
 
+    @Step("Edit VLAN Interface and set MTU {mtu} and description {description}")
+    public void editVLANInterface(String mtu, String description) {
+        waitForPageToLoad();
+        getWizard().setComponentValue(IRB_MTU_ID, mtu, NUMBER_FIELD);
+        waitForPageToLoad();
+        getWizard().setComponentValue(IRB_DESCRIPTION_ID, description, TEXT_FIELD);
+        waitForPageToLoad();
+        getWizard().clickAccept();
+    }
+
     private Wizard getWizard() {
         return Wizard.createByComponentId(driver, wait, WIZARD_ID);
+    }
+
+    private void waitForPageToLoad() {
+        DelayUtils.waitForPageToLoad(driver, wait);
     }
 }
