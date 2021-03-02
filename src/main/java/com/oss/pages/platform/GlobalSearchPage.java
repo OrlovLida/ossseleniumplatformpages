1d00abd5df962d41cec78ebd12f91a751921e912
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2021-03-02 13:11:10 +0100

    ER OSSTEST-103003 # add objectType search in GlobalSearchPage # correct UC_NAR_004 tests

--- src/main/java/com/oss/pages/platform/GlobalSearchPage.java
+++ src/main/java/com/oss/pages/platform/GlobalSearchPage.java
@@ -1,14 +1,20 @@
 package com.oss.pages.platform;
 
+import org.openqa.selenium.WebDriver;
+
+import com.oss.framework.components.inputs.ComponentFactory;
+import com.oss.framework.components.inputs.Input;
+import com.oss.framework.components.inputs.Input.ComponentType;
 import com.oss.framework.components.portals.DropdownList;
 import com.oss.framework.listwidget.CommonList;
 import com.oss.pages.BasePage;
+
 import io.qameta.allure.Step;
-import org.openqa.selenium.WebDriver;
 
 public class GlobalSearchPage extends BasePage {
 
     private static final String OBJECTS_LIST_DATA_ATTRIBUTE_NAME = "objectsList";
+    private static final String OBJECT_TYPE_FILTER_COMBOBOX_ID = "filteringWidget";
 
     public GlobalSearchPage(WebDriver driver) {
         super(driver);
@@ -20,6 +26,12 @@ public class GlobalSearchPage extends BasePage {
         DropdownList threeDotsList = DropdownList.create(driver, wait);
         threeDotsList.selectOptionWithId(option);
         threeDotsList.selectOptionWithId(viewName);
+    }
+
+    @Step("Filter by object type {objectType}")
+    public void filterObjectType(String objectType) {
+        Input input = ComponentFactory.create(OBJECT_TYPE_FILTER_COMBOBOX_ID, ComponentType.COMBOBOXV2, driver, wait);
+        input.setSingleStringValue(objectType);
     }
 
     public CommonList getResultsList() {
