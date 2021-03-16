4e30350f48f036a894633ec657d696fc33cfab3b
Jakub Skwara <jakub.skwara@comarch.com>
2021-03-16 17:01:34 +0100

    ER BIGDATA-4846 Usage of framework library for Selenium testing # after Editable List refactoring

--- src/main/java/com/oss/pages/bigdata/dfe/stepwizard/commons/ColumnMappingPage.java
+++ src/main/java/com/oss/pages/bigdata/dfe/stepwizard/commons/ColumnMappingPage.java
@@ -4,7 +4,6 @@ import com.oss.framework.components.inputs.Input.ComponentType;
 import com.oss.framework.listwidget.EditableList;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ColumnMappingPage extends BaseStepPage {
@@ -23,17 +22,18 @@ public class ColumnMappingPage extends BaseStepPage {
     public void fillColumnMappingStep(String columnNameValue, String columnRole, String linkageToDimensionTable){
         EditableList columnMappingList = EditableList.create(driver, wait);
 
-        WebElement row = columnMappingList.selectRowByStaticAttributeValue(COLUMN_DATA_ID, columnNameValue);
-        columnMappingList.setValue(row, COLUMN_ROLE_ID, columnRole, COLUMN_ROLE_INPUT_ID, ComponentType.COMBOBOX);
-        columnMappingList.setValue(row, LINKAGE_TO_DIMENSION_TABLE_ID, linkageToDimensionTable, LINKAGE_TO_DIMENSION_TABLE_INPUT_ID, ComponentType.COMBOBOX);
+        EditableList.Row row = columnMappingList.selectRowByStaticAttributeValue(COLUMN_DATA_ID, columnNameValue);
+
+        row.setEditableAttributeValue(columnRole, COLUMN_ROLE_ID, COLUMN_ROLE_INPUT_ID, ComponentType.COMBOBOX);
+        row.setEditableAttributeValue(linkageToDimensionTable, LINKAGE_TO_DIMENSION_TABLE_ID, LINKAGE_TO_DIMENSION_TABLE_INPUT_ID, ComponentType.COMBOBOX);
     }
 
     @Step("I fill Column Mapping Step. Linkage to dimension table: {linkageToDimensionTable} for column: {columnNameValue}")
     public void fillColumnMappingStep(String columnNameValue, String linkageToDimensionTable){
         EditableList columnMappingList = EditableList.create(driver, wait);
 
-        WebElement row = columnMappingList.selectRowByStaticAttributeValue(COLUMN_DATA_ID, columnNameValue);
-        columnMappingList.setValue(row, LINKAGE_TO_DIMENSION_TABLE_ID, linkageToDimensionTable, LINKAGE_TO_DIMENSION_TABLE_INPUT_ID, ComponentType.COMBOBOX);
+        EditableList.Row row = columnMappingList.selectRowByStaticAttributeValue(COLUMN_DATA_ID, columnNameValue);
+        row.setEditableAttributeValue(linkageToDimensionTable, LINKAGE_TO_DIMENSION_TABLE_ID, LINKAGE_TO_DIMENSION_TABLE_INPUT_ID, ComponentType.COMBOBOX);
     }
 
 }
