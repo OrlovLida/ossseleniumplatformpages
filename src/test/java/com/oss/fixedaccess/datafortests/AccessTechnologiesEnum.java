c0368a0e219a53b880153c14dac4bf84468904c7
Mateusz Mikołajczyk <mateusz.mikolajczyk@comarch.com>
2021-06-29 12:21:41 +0200

    - Add enum class for Access technologies - Add Class for creation simple structure for SQ tests

---
+++ src/test/java/com/oss/fixedaccess/datafortests/AccessTechnologiesEnum.java
@@ -0,0 +1,52 @@
+package com.oss.fixedaccess.datafortests;
+
+
+public enum AccessTechnologiesEnum {
+
+    GPON("GPON", "2547.7", "1273,8", "", "Fiber"),
+    ONE_G_EPON("1G-EPON", "1280.0", "1280.0", "1280.0", "Fiber"),
+    TEN_G_EPON("10G-EPON", "10560.0", "1280.0", "", "Fiber"),
+    TEN_G_ONE_G_EPON("10G-1G-EPON", "10560.0", "10560.0", "10560.0", "Fiber"),
+    VDSL("VDSL", "55.0", "3", "", "Pair"),
+    VDSL_TWO("VDSL2", "40.0", "3", "", "Pair"),
+    ADSL("ADSL", "8.0", "1.8", "", "Pair"),
+    ADSL_TWO("ADSL2", "12.0", "3.5", "", "Pair"),
+    ADSL_TWO_PLUS("ADSL2+", "10.0", "2", "", "Pair"),
+    DOCSIS_TWO_POINT_ZERO("DOCSIS 2.0", "40.0", "30.0", "", "Coax"),
+    DOCSIS_THREE_POINT_ZERO("DOCSIS 3.0", "1280.0", "200", "", "Coax"),
+    DOCSIS_THREE_POINT_ONE("DOCSIS 3.1", "10000.0", "2000", "", "Coax");
+
+    private String name;
+    private String downloadSpeed;
+    private String uploadSpeed;
+    private String maxBandwidth;
+    private String supportedMediumType;
+
+    AccessTechnologiesEnum(String name, String downloadSpeed, String uploadSpeed, String maxBandwidth, String supportedMediumType) {
+        this.name = name;
+        this.downloadSpeed = downloadSpeed;
+        this.uploadSpeed = uploadSpeed;
+        this.maxBandwidth = maxBandwidth;
+        this.supportedMediumType = supportedMediumType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDownloadSpeed() {
+        return downloadSpeed;
+    }
+
+    public String getUploadSpeed() {
+        return uploadSpeed;
+    }
+
+    public String getMaxBandwidth() {
+        return maxBandwidth;
+    }
+
+    public String getSupportedMediumType() {
+        return supportedMediumType;
+    }
+}
