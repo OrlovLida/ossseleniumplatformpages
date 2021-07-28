0e99948cb23566e47d302ce25aa63390a613eba1
Paweł Rother <Pawel.Rother@comarch.com>
2021-07-28 08:49:16 +0200

    ER OSSPLA-11365 #Add ProcessModelsPage class with import/export model tests. #Add EditMilestoneDefinitionPage

---
+++ src/main/java/com/oss/pages/bpm/EditMilestoneDefinitionPage.java
@@ -0,0 +1,21 @@
+package com.oss.pages.bpm;
+
+import com.oss.framework.alerts.SystemMessageContainer;
+import com.oss.framework.alerts.SystemMessageInterface;
+import com.oss.pages.BasePage;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.WebDriver;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class EditMilestoneDefinitionPage extends BasePage {
+
+    public EditMilestoneDefinitionPage(WebDriver driver) {super(driver);}
+
+    public void byleco (){
+
+    }
+
+}
