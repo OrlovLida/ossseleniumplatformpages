49cb5c6e4e6487db78d7999c6286683296448d7d
Anna.Kolczyk <anna.kolczyk@comarch.com>
2021-09-08 09:07:45 +0200

    minor changes to tests, add wait before click search

--- src/test/java/com/oss/bigdata/dfe/SMOKE/XDRBrowserTest.java
+++ src/test/java/com/oss/bigdata/dfe/SMOKE/XDRBrowserTest.java
@@ -3,14 +3,18 @@ package com.oss.bigdata.dfe.SMOKE;
 import com.oss.BaseTestCase;
 import com.oss.pages.bigdata.dfe.XDRBrowserPage;
 import io.qameta.allure.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class XDRBrowserTest extends BaseTestCase {
 
+    private static final Logger log = LoggerFactory.getLogger(XDRBrowserTest.class);
+
     private XDRBrowserPage xdrBrowserPage;
-    private static final String ETL_NAME = "self:AGGR#AggregationProcesses";
+    private static final String ETL_NAME = "t:SMOKE#ETLforKqis";
 
     @BeforeClass
     public void goToXDRBrowserView() {
@@ -21,7 +25,7 @@ public class XDRBrowserTest extends BaseTestCase {
     @Description("Search in XDR Browser for ETL")
     public void searchInXDRBrowser() {
         xdrBrowserPage.selectETLName(ETL_NAME);
-        xdrBrowserPage.setValueInTimePeriodChooser(1, 1, 1);
+        xdrBrowserPage.setValueInTimePeriodChooser(3, 1, 1);
         xdrBrowserPage.clickSearch();
 
         Assert.assertFalse(xdrBrowserPage.checkIfTableIsEmpty(), "XDR Table is empty!");
@@ -30,16 +34,21 @@ public class XDRBrowserTest extends BaseTestCase {
     @Test(priority = 2, testName = "exportXDRFile", description = "Export XDR File")
     @Description("Export XDR File")
     public void exportXDRFile() {
-        xdrBrowserPage.clearNotifications();
-        xdrBrowserPage.clickExport();
-        xdrBrowserPage.clickOnNotificationIcon();
+        if (!xdrBrowserPage.checkIfTableIsEmpty()) {
+            xdrBrowserPage.clearNotifications();
+            xdrBrowserPage.clickExport();
+            xdrBrowserPage.clickOnNotificationIcon();
 
-        Assert.assertEquals(xdrBrowserPage.amountOfNotifications(), 1);
-        xdrBrowserPage.clickDownload();
-        xdrBrowserPage.attachDownloadedFileToReport();
-        xdrBrowserPage.clearNotifications();
+            Assert.assertEquals(xdrBrowserPage.amountOfNotifications(), 1);
+            xdrBrowserPage.clickDownload();
+            xdrBrowserPage.attachDownloadedFileToReport();
+            xdrBrowserPage.clearNotifications();
 
-        Assert.assertEquals(xdrBrowserPage.amountOfNotifications(), 0);
-        Assert.assertTrue(xdrBrowserPage.checkIfFileIsNotEmpty());
+            Assert.assertEquals(xdrBrowserPage.amountOfNotifications(), 0);
+            Assert.assertTrue(xdrBrowserPage.checkIfFileIsNotEmpty());
+        } else {
+            log.error("ETL with name: {} has no data in XDR Table", ETL_NAME);
+            Assert.fail();
+        }
     }
 }
