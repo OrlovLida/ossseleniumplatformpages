7f8d1661656d70be44a83950968288ce81b0d8f2
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2021-12-08 21:25:04 +0100

    ER OSSTEST-112457 # Wizard corrections # AttachFileWizardPage corrections

--- src/main/java/com/oss/pages/dms/AttachFileWizardPage.java
+++ src/main/java/com/oss/pages/dms/AttachFileWizardPage.java
@@ -9,6 +9,7 @@ package com.oss.pages.dms;
 import java.util.List;
 
 import org.openqa.selenium.WebDriver;
+
 import com.oss.framework.components.inputs.Input;
 import com.oss.framework.utils.DelayUtils;
 import com.oss.framework.widgets.Wizard;
@@ -18,53 +19,50 @@ import com.oss.pages.BasePage;
  * @author Gabriela Kasza
  */
 public class AttachFileWizardPage extends BasePage {
-    
-    private static final String WIZARD_ID = "addFileComponentId";
+
+    private static final String WIZARD_ID = "addFileViewId";
     private static final String FILE_COMPONENT_ID = "file";
     private static final String NEXT_BUTTON_ID = "wizard-next-button-addFileComponentId";
     private static final String ACCEPT_BUTTON_ID = "wizard-submit-button-addFileComponentId";
     private static final String RADIO_BUTTONS_ID = "duplicateFilesRadioButtons";
-    
+
     private Wizard addFileWizard = Wizard.createByComponentId(driver, wait, WIZARD_ID);
-    
+
     public AttachFileWizardPage(WebDriver driver) {
         super(driver);
     }
-    
+
     public void attachFile(String filePath) {
         Input input = addFileWizard.getComponent(FILE_COMPONENT_ID, Input.ComponentType.FILE_CHOOSER);
         input.setSingleStringValue(filePath);
     }
-    
+
     public void nextButton() {
         Wizard.createByComponentId(driver, wait, WIZARD_ID).clickActionById(NEXT_BUTTON_ID);
     }
-    
+
     public void acceptButton() {
         addFileWizard.clickActionById(ACCEPT_BUTTON_ID);
-        
     }
-    
+
     public void deleteFiles() {
         Input input = addFileWizard.getComponent(FILE_COMPONENT_ID, Input.ComponentType.FILE_CHOOSER);
         input.clear();
-        
     }
-    
+
     public List<String> getAttachmentName() {
         Input input = addFileWizard.getComponent(FILE_COMPONENT_ID, Input.ComponentType.FILE_CHOOSER);
         return input.getStringValues();
     }
-    
+
     public void selectRadioButton(String label) {
         Input radio = Wizard.createByComponentId(driver, wait, WIZARD_ID)
                 .getComponent(RADIO_BUTTONS_ID, Input.ComponentType.RADIO_BUTTON);
         radio.setSingleStringValue(label);
         DelayUtils.sleep();
     }
-    
+
     public void skipAndAccept() {
         Wizard.createByComponentId(driver, wait, WIZARD_ID).skipAndAccept(ACCEPT_BUTTON_ID);
     }
-    
 }
