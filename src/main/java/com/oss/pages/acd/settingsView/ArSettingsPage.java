1ff918323d8947a39c4bbcad9243bf34ef62bc39
weronika.pawlowicz <weronika.pawlowicz@comarch.com>
2021-12-31 18:37:47 +0100

    ER-ACD-3090 tests for ACD - AR Settings View

---
+++ src/main/java/com/oss/pages/acd/settingsView/ArSettingsPage.java
@@ -0,0 +1,149 @@
+package com.oss.pages.acd.settingsView;
+
+import com.oss.framework.components.contextactions.ButtonContainer;
+import com.oss.framework.components.inputs.Button;
+import com.oss.framework.components.inputs.ComponentFactory;
+import com.oss.framework.components.inputs.Input;
+import com.oss.framework.components.inputs.SearchField;
+import com.oss.framework.prompts.ConfirmationBox;
+import com.oss.framework.prompts.ConfirmationBoxInterface;
+import com.oss.framework.utils.CSSUtils;
+import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.widgets.Wizard;
+import com.oss.framework.widgets.tablewidget.OldTable;
+import com.oss.framework.widgets.tabswidget.TabWindowWidget;
+import com.oss.framework.widgets.tabswidget.TabsInterface;
+import com.oss.framework.widgets.tabswidget.TabsWidget;
+import com.oss.pages.acd.BaseACDPage;
+import com.oss.pages.acd.scenarioSummaryView.AsdScenarioSummaryViewPage;
+
+import io.qameta.allure.Step;
+
+import org.apache.logging.log4j.core.util.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArSettingsPage extends BaseACDPage {
+
+    private static final Logger log = LoggerFactory.getLogger(AsdScenarioSummaryViewPage.class);
+
+    private final String ADD_ACTION_TEMPLATE_BUTTON = "undefined-1";
+    private final String REMOVE_ACTION_TEMPLATE_BUTTON = "undefined-0";
+    private final String ACTION_TEMPLATE_TABLE_ID = "actionTemplateTable";
+    private final String DELETE_LABEL = "Delete";
+
+    private final OldTable table;
+
+    public ArSettingsPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+        table = OldTable.createByComponentDataAttributeName(driver, wait, ACTION_TEMPLATE_TABLE_ID);
+    }
+
+    @Step("I Open AR Settings View")
+    public static ArSettingsPage goToPage(WebDriver driver, String suffixURL, String basicURL) {
+        WebDriverWait wait = new WebDriverWait(driver, 150);
+
+        String pageUrl = String.format(suffixURL, basicURL);
+        driver.get(pageUrl);
+        DelayUtils.waitForPageToLoad(driver, wait);
+        log.info("Opened page: {}", pageUrl);
+
+        return new ArSettingsPage(driver, wait);
+    }
+
+    @Step("Click add new Action Template")
+    public void clickAddActionTemplate() {
+        Button button = Button.createByXpath(ADD_ACTION_TEMPLATE_BUTTON, "li", CSSUtils.DATA_WIDGET_ID, driver);
+        button.click();
+        log.info("Clicking Add new Action Template button");
+    }
+
+    @Step("I set value in ComboBox")
+    public void chooseOptionInComboBox(String comboBoxId, String value) {
+        DelayUtils.waitForPageToLoad(driver, wait);
+        Wizard.createWizard(driver, wait).setComponentValue(comboBoxId, value, Input.ComponentType.COMBOBOX);
+        log.info("Setting value in ComboBox: {}", value);
+    }
+
+    @Step("I set description")
+    public void setValueInInputField(String fieldName, String value) {
+        ComponentFactory.create(fieldName, Input.ComponentType.TEXT_FIELD, driver, wait).setSingleStringValue(value);
+        DelayUtils.sleep();
+    }
+
+    @Step("I click accept button")
+    public void clickSaveButton() {
+        ConfirmationBoxInterface prompt = ConfirmationBox.create(driver, wait);
+        prompt.clickButtonByLabel("Save");
+    }
+
+    @Step("I search for Action Template")
+    public Boolean searchingThroughActionTemplates(String comboBoxId, String value) {
+        DelayUtils.waitForPageToLoad(driver, wait);
+
+        if (!isDataInActionTemplatesTable()) {
+            log.info("Action Template table has no data");
+            return false;
+        }
+        ComponentFactory.create(comboBoxId, Input.ComponentType.MULTI_SEARCH_FIELD, driver, wait).setSingleStringValue(value);
+        log.info("I am searching for created Action Template");
+        return true;
+    }
+
+    @Step("Check if created Action Template exists")
+    public Boolean isThereActionTemplateCreated() {
+        log.info("I am in isThereActionTemplateCreated method");
+
+        DelayUtils.waitForPageToLoad(driver, wait);
+        String firstActionIdInTable = table.getCellValue(0, "Action Id");
+
+        if (!isDataInActionTemplatesTable()) {
+            log.info("Action Template table has no data");
+            return false;
+        }
+        log.info("Action Id of newly created object is = {} ", firstActionIdInTable);
+        return true;
+    }
+
+    @Step("I check if there is data in Action Template table")
+    public Boolean isDataInActionTemplatesTable() {
+        DelayUtils.waitForPageToLoad(driver, wait);
+        log.info("I am checking if there is data in Action Templates table");
+        return !table.hasNoData();
+    }
+
+    @Step("I select Action Template")
+    public void selectFirstActionTemplateFromTable() {
+        DelayUtils.waitForPageToLoad(driver, wait);
+        log.info("I am selecting Action Template");
+        OldTable.createByComponentId(driver, wait, ACTION_TEMPLATE_TABLE_ID).selectRow(0);
+    }
+
+    @Step("I click delete Action Template")
+    public Boolean deleteActionTemplate() {
+        DelayUtils.waitForPageToLoad(driver, wait);
+        log.info("I am clicking Remove Action Template button");
+        Button deleteButton = Button.createByXpath(REMOVE_ACTION_TEMPLATE_BUTTON, "li", CSSUtils.DATA_WIDGET_ID, driver);
+        deleteButton.click();
+        log.info("I am confirming removal of Action Template");
+        ConfirmationBox.create(driver, wait).clickButtonByLabel(DELETE_LABEL);
+
+        log.info("I am checking if Action Template has been removed");
+        if (isDataInActionTemplatesTable()) {
+            log.info("Action Template table is not empty - table still contains data.");
+            return false;
+        }
+        log.info("Table is empty. Action Template has been removed successfully");
+        return true;
+    }
+
+    @Step("I go to Subsystems Health tab")
+    public void goToSubsystemsHealthTab(String tabLabel) {
+        DelayUtils.waitForPageToLoad(driver, wait);
+        TabsWidget.create(driver, wait).selectTabByLabel(tabLabel);
+        log.debug("I opened Subsystems Health tab");
+    }
+
+}
---
+++ src/test/java/com/oss/acd/ArSettingsTest.java
@@ -0,0 +1,88 @@
+package com.oss.acd;
+
+import com.oss.BaseTestCase;
+import com.oss.framework.components.inputs.Button;
+import com.oss.pages.acd.BaseACDPage;
+import com.oss.pages.acd.settingsView.ArSettingsPage;
+import com.oss.utils.TestListener;
+
+import io.qameta.allure.Description;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners({TestListener.class})
+public class ArSettingsTest extends BaseTestCase {
+
+    private static final Logger log = LoggerFactory.getLogger(AsdScenarioSummaryViewTest.class);
+
+    private ArSettingsPage arSettingsPage;
+    private BaseACDPage baseACDPage;
+
+    private final String arSettingsViewSuffixUrl = "%s/#/view/acd/kaSettings";
+    private String TYPE_ACTION_TEMPLATE_COMBOBOX_ID = "type-input";
+    private String REASON_ACTION_TEMPLATE_COMBOBOX_ID = "reason-input";
+    private String SCENARIO_ACTION_TEMPLATE_COMBOBOX_ID = "scenario-input";
+    private String ACTION_TEMPLATE_TYPE_VALUE = "Software";
+    private String ACTION_TEMPLATE_REASON_VALUE = "Fix";
+    private String ALL_SCENARIOS = "ALL";
+    private String DESCRIPTION_COMBO_BOX = "description";
+    private String DESCRIPTION_COMBO_BOX_VALUE = "Test123!?@Desc";
+    private String PARAM_COMBO_BOX = "param";
+    private String PARAM_COMBO_BOX_VALUE = "Test123!?@Param";
+    private String SUBSYSTEMS_HEALTH_TAB = "Subsystems Health";
+
+    @BeforeClass
+    public void goToArSettingsView() {
+        arSettingsPage = ArSettingsPage.goToPage(driver, arSettingsViewSuffixUrl, BASIC_URL);
+        baseACDPage = new BaseACDPage(driver, webDriverWait);
+    }
+
+    @Test(priority = 1, testName = "Add new Action Template", description = "Add new Action Template")
+    @Description("Add new Action Template")
+    public void addNewActionTemplate() {
+        log.info("Waiting in method addNewActionTemplate");
+        arSettingsPage.clickAddActionTemplate();
+        arSettingsPage.chooseOptionInComboBox(TYPE_ACTION_TEMPLATE_COMBOBOX_ID, ACTION_TEMPLATE_TYPE_VALUE);
+        arSettingsPage.chooseOptionInComboBox(REASON_ACTION_TEMPLATE_COMBOBOX_ID, ACTION_TEMPLATE_REASON_VALUE);
+        arSettingsPage.setValueInInputField(DESCRIPTION_COMBO_BOX, DESCRIPTION_COMBO_BOX_VALUE);
+        arSettingsPage.setValueInInputField(PARAM_COMBO_BOX, PARAM_COMBO_BOX_VALUE);
+        arSettingsPage.chooseOptionInComboBox(SCENARIO_ACTION_TEMPLATE_COMBOBOX_ID, ALL_SCENARIOS);
+        arSettingsPage.clickSaveButton();
+        log.info("Action Template has been created");
+    }
+
+    @Test(priority = 2, testName = "Verify if created Action Template exists", description = "Verify if created Action Template exists")
+    @Description("Verify if created Action Template exists")
+    public void searchingForActionTemplate() {
+
+        if (!arSettingsPage.searchingThroughActionTemplates(DESCRIPTION_COMBO_BOX, DESCRIPTION_COMBO_BOX_VALUE)) {
+            log.error("Action Template table is empty");
+            Assert.fail();
+        }
+
+        if (!arSettingsPage.isThereActionTemplateCreated()) {
+            log.error("Action Template table doesn't contain data for provided filters");
+            Assert.fail();
+        }
+        log.info("Action Template has been found");
+    }
+
+    @Test(priority = 3, testName = "Delete Action Template", description = "Delete Action Template")
+    @Description("Delete Action Template")
+    public void deleteActionTemplate() {
+        arSettingsPage.selectFirstActionTemplateFromTable();
+        arSettingsPage.deleteActionTemplate();
+
+    }
+
+    @Test(priority = 4, testName = "Check Subsystems Health", description = "Check Subsystems Health")
+    @Description("Check Subsystems Health")
+    public void checkSubsystemsHealth() {
+        arSettingsPage.goToSubsystemsHealthTab(SUBSYSTEMS_HEALTH_TAB);
+    }
+}
