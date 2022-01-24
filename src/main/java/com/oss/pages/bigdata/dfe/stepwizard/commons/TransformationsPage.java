761a27e457c8ba619edf8e13796e705bdd009b11
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2022-01-24 19:02:52 +0100

    ER OSSWEB-16050 # Widget corrections

--- src/main/java/com/oss/pages/bigdata/dfe/stepwizard/commons/TransformationsPage.java
+++ src/main/java/com/oss/pages/bigdata/dfe/stepwizard/commons/TransformationsPage.java
@@ -1,26 +1,29 @@
 package com.oss.pages.bigdata.dfe.stepwizard.commons;
 
-import com.oss.framework.utils.DelayUtils;
-import com.oss.framework.wizard.Wizard;
-import com.oss.framework.iaa.widget.dfe.transformationsmanager.TransformationsManagerWidget;
-import com.oss.pages.BasePage;
-import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.oss.framework.iaa.widget.dfe.transformationsmanager.TransformationsManagerWidget;
+import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.wizard.Wizard;
+import com.oss.pages.BasePage;
+
+import io.qameta.allure.Step;
+
 public class TransformationsPage extends BasePage {
 
     private static final Logger log = LoggerFactory.getLogger(TransformationsPage.class);
+    private static final String WIZARD_ID = "etlWizardWindow";
     private final Wizard transformationWizard;
 
     public TransformationsPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
-        transformationWizard = Wizard.createWizard(driver, wait);
+        transformationWizard = Wizard.createByComponentId(driver, wait, WIZARD_ID);
     }
 
-    public void addNewTransformation(String transformationName){
+    public void addNewTransformation(String transformationName) {
         TransformationsManagerWidget transformationsManager = TransformationsManagerWidget.create(driver, wait);
         transformationsManager.selectTransformation(transformationName);
         transformationsManager.clickAdd();
