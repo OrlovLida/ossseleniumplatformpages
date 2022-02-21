169b17ffa3f073a3c85c4de3e21a8fd11b8d03c1
Anna Kolczyk <anna.kolczyk@comarch.com>
2022-02-21 13:15:55 +0000

    add downloading on NotificationWrapperPage

--- src/main/java/com/oss/pages/bigdata/dfe/datasource/DSWizard/DataSourceSpecificInfoPage.java
+++ src/main/java/com/oss/pages/bigdata/dfe/datasource/DSWizard/DataSourceSpecificInfoPage.java
@@ -1,4 +1,4 @@
-package com.oss.pages.bigdata.dfe.DataSource.DSWizard;
+package com.oss.pages.bigdata.dfe.datasource.DSWizard;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
