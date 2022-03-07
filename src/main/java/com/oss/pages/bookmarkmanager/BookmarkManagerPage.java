be2a5ed48fa7d7f44d6bd1aa6b1164d717b63d32
Anna.Kolczyk <anna.kolczyk@comarch.com>
2022-03-07 11:17:51 +0100

    Bookmark test - master

--- src/main/java/com/oss/pages/bookmarkmanager/BookmarkManagerPage.java
+++ src/main/java/com/oss/pages/bookmarkmanager/BookmarkManagerPage.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.oss.framework.components.contextactions.ButtonContainer;
+import com.oss.framework.components.inputs.Button;
 import com.oss.framework.components.inputs.ComponentFactory;
 import com.oss.framework.components.inputs.Input;
 import com.oss.framework.utils.DelayUtils;
@@ -15,53 +16,77 @@ import com.oss.pages.BasePage;
 import io.qameta.allure.Step;
 
 public class BookmarkManagerPage extends BasePage {
-    
+
     private static final Logger log = LoggerFactory.getLogger(BookmarkManagerPage.class);
     private static final String SEARCH_FIELD_ID = "bookmarkSearch";
     private static final String LIST_ID = "bookmarksList";
     private static final String ROW_ATTRIBUTE_NAME = "Name";
     private static final String LINK_TEXT = "Open";
     private static final String CREATE_NEW_CATEGORY_LABEL = "New category";
-    
+    private static final String DELETE_LABEL = "Delete";
+    private static final String CONFIRM_DELETE_BUTTON_ID = "ConfirmationBox_removeBookmarkConfirmationBox_action_button";
+
     public BookmarkManagerPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
-    
-    @Step("I go to Bookmark Manager page")
+
+    @Step("Go to Bookmark Manager page")
     public static BookmarkManagerPage goToPage(WebDriver driver, String basicURL) {
         WebDriverWait wait = new WebDriverWait(driver, 45);
+
         String pageUrl = String.format("%s/#/view/bookmarkmanager/main", basicURL);
         driver.get(pageUrl);
         DelayUtils.waitForPageToLoad(driver, wait);
         log.info("Opened page: {}", pageUrl);
+
         return new BookmarkManagerPage(driver, wait);
     }
-    
-    @Step("I search for bookmark")
+
+    @Step("Search for bookmark")
     public void searchForBookmark(String bookmarkName) {
-        Input search = ComponentFactory.create(SEARCH_FIELD_ID, Input.ComponentType.SEARCH_BOX, driver, wait);
-        search.setSingleStringValue(bookmarkName);
+        ComponentFactory.create(SEARCH_FIELD_ID, Input.ComponentType.SEARCH_BOX, driver, wait)
+                .setSingleStringValue(bookmarkName);
         log.debug("Searching bookmark {}", bookmarkName);
     }
-    
-    @Step("I expand found category")
+
+    @Step("Expand found category")
     public void expandBookmarkList(String categoryName) {
-        CommonList.create(driver, wait, LIST_ID).expandCategory(categoryName);
+        getBookmarkList().expandCategory(categoryName);
         log.info("I expanded category: {}", categoryName);
     }
-    
-    @Step("I open bookmark")
+
+    @Step("Open bookmark")
     public void openBookmark(String bookmarkName) {
-        CommonList.create(driver, wait, LIST_ID).getRow(ROW_ATTRIBUTE_NAME, bookmarkName).clickLink(LINK_TEXT);
+        getBookmarkList().getRow(ROW_ATTRIBUTE_NAME, bookmarkName).clickLink(LINK_TEXT);
+        DelayUtils.waitForPageToLoad(driver, wait);
     }
-    
+
     @Step("I check if list contains any bookmarks")
     public boolean isAnyBookmarkInList() {
         DelayUtils.waitForPageToLoad(driver, wait);
-        return !CommonList.create(driver, wait, LIST_ID).hasNoData();
+        return !getBookmarkList().hasNoData();
     }
-    
+
+    @Step("Click Create New Category Button")
     public void clickCreateNewCategory() {
         ButtonContainer.create(driver, wait).callActionByLabel(CREATE_NEW_CATEGORY_LABEL);
+        log.info("Clicking Create New Category");
+    }
+
+    @Step("Click Delete Bookmark")
+    public void clickDeleteBookmark(String bookmarkName) {
+        getBookmarkList().getRow(ROW_ATTRIBUTE_NAME, bookmarkName).callActionByLabel(DELETE_LABEL);
+        DelayUtils.waitForPageToLoad(driver, wait);
+        Button.createById(driver, CONFIRM_DELETE_BUTTON_ID).click();
+        DelayUtils.waitForPageToLoad(driver, wait);
+    }
+
+    @Step("Check if bookmark is deleted")
+    public boolean isBookmarkDeleted(String bookmarkName) {
+        return !getBookmarkList().isRowDisplayed(ROW_ATTRIBUTE_NAME, bookmarkName);
+    }
+
+    private CommonList getBookmarkList() {
+        return CommonList.create(driver, wait, LIST_ID);
     }
 }
