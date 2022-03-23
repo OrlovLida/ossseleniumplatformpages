4b87fd6c3fe051852b9202be4e8715a495364e1c
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2022-03-23 15:44:16 +0100

    ER OSSTEST-115349 # add SearchObjectTypePage

---
+++ src/main/java/com/oss/pages/platform/SearchObjectTypePage.java
@@ -0,0 +1,39 @@
+package com.oss.pages.platform;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.oss.framework.components.inputs.Button;
+import com.oss.framework.components.inputs.ComponentFactory;
+import com.oss.framework.components.inputs.Input;
+import com.oss.pages.BasePage;
+
+import static com.oss.framework.components.inputs.Input.ComponentType.COMBOBOX;
+
+public class SearchObjectTypePage extends BasePage {
+
+    private static final String TYPE_INPUT_ID = "type-chooser-input";
+    private static final String OPEN_BUTTON_ID = "open-button";
+
+    public SearchObjectTypePage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public void searchType(String type) {
+        getSearchTypeInput().setSingleStringValue(type);
+        clickOpen();
+    }
+
+    public void searchTypeContains(String type) {
+        getSearchTypeInput().setSingleStringValueContains(type);
+        clickOpen();
+    }
+
+    private void clickOpen() {
+        Button.createById(driver, OPEN_BUTTON_ID).click();
+    }
+
+    private Input getSearchTypeInput() {
+        return ComponentFactory.create(TYPE_INPUT_ID, COMBOBOX, driver, wait);
+    }
+}
