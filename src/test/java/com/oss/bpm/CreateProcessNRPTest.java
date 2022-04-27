73bcf74951bc3cabd43176ee43c539b1cf6f0b95
Paweł Rother <pawel.rother@comarch.com>
2022-04-27 13:54:19 +0200

    ER OSSPLA-13519 Maintenance

--- src/test/java/com/oss/bpm/CreateProcessNRPTest.java
+++ src/test/java/com/oss/bpm/CreateProcessNRPTest.java
@@ -604,7 +604,7 @@ public class CreateProcessNRPTest extends BaseTestCase {
     @AfterClass()
     public void switchToLivePerspective() {
         PerspectiveChooser perspectiveChooser = PerspectiveChooser.create(driver, webDriverWait);
-        if (!perspectiveChooser.getCurrentPerspective().equals("Live"))
+        if (!perspectiveChooser.getCurrentPerspective().equalsIgnoreCase("live"))
             perspectiveChooser.setLivePerspective();
     }
 
