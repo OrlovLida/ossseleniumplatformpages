738cfdd9d101803e98fc191f85480da70c79fe2a
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2022-05-24 15:31:13 +0200

    ER OSSTEST-117908 # E2E corrections

--- src/test/java/com/oss/E2E/ISPConfigurationTest.java
+++ src/test/java/com/oss/E2E/ISPConfigurationTest.java
@@ -522,8 +522,8 @@ public class ISPConfigurationTest extends BaseTestCase {
     @Description("Create 3 footprints and check confirmation system message")
     public void createFootprint() {
         LocationOverviewPage locationOverviewPage = new LocationOverviewPage(driver);
-        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "rh01");
-        locationOverviewPage.clickButtonByLabelInSpecificTab(TabName.LOCATIONS, "Create Sublocation");
+        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "Row1");
+        locationOverviewPage.clickActionById(TabName.LOCATIONS, "frameworkCustomMore_sublocation-create-action");
         SublocationWizardPage sublocationWizardPage = new SublocationWizardPage(driver);
         sublocationWizardPage.setSublocationType("Footprint");
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
@@ -545,7 +545,7 @@ public class ISPConfigurationTest extends BaseTestCase {
         locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, MOUNTING_EDITOR_NAME);
         locationOverviewPage.clickButtonByLabelInSpecificTab(TabName.LOCATIONS, "Edit Location");
         SublocationWizardPage sublocationWizardPage = new SublocationWizardPage(driver);
-        sublocationWizardPage.setPreciseLocation("fp01");
+        sublocationWizardPage.setPreciseLocation("Footprint1");
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
         sublocationWizardPage.clickAccept();
         checkPopupAndCloseMessage();
@@ -562,7 +562,7 @@ public class ISPConfigurationTest extends BaseTestCase {
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
         deviceWizardPage.nextUpdateWizard();
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
-        deviceWizardPage.setPreciseLocation("fp03");
+        deviceWizardPage.setPreciseLocation("Footprint3");
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
         deviceWizardPage.acceptUpdateWizard();
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
@@ -656,17 +656,17 @@ public class ISPConfigurationTest extends BaseTestCase {
         LocationOverviewPage locationOverviewPage = new LocationOverviewPage(driver);
         locationOverviewPage.selectTab("Locations");
 
-        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "fp01");
+        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "Footprint1");
         locationOverviewPage.clickButtonByLabelInSpecificTab(TabName.LOCATIONS, "Delete Location");
         locationOverviewPage.clickButtonInConfirmationBox("Delete");
         checkPopupAndCloseMessage();
 
-        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "fp02");
+        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "Footprint2");
         locationOverviewPage.clickButtonByLabelInSpecificTab(TabName.LOCATIONS, "Delete Location");
         locationOverviewPage.clickButtonInConfirmationBox("Delete");
         checkPopupAndCloseMessage();
 
-        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "fp03");
+        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "Footprint3");
         locationOverviewPage.clickButtonByLabelInSpecificTab(TabName.LOCATIONS, "Delete Location");
         locationOverviewPage.clickButtonInConfirmationBox("Delete");
         checkPopupAndCloseMessage();
@@ -676,7 +676,7 @@ public class ISPConfigurationTest extends BaseTestCase {
     @Description("Delete row and check confirmation system message")
     public void deleteRow() {
         LocationOverviewPage locationOverviewPage = new LocationOverviewPage(driver);
-        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "rh01");
+        locationOverviewPage.filterObjectInSpecificTab(TabName.LOCATIONS, NAME, "Row1");
         locationOverviewPage.clickButtonByLabelInSpecificTab(TabName.LOCATIONS, "Delete Location");
         locationOverviewPage.clickButtonInConfirmationBox("Delete");
         checkPopupAndCloseMessage();
