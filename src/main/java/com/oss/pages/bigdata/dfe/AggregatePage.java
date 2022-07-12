ab1a3547b247cfd5fd3c7a79fb0927a3a4543a78
Kinga Balcar-Mazur <kinga.balcar-mazur@comarch.com>
2022-07-12 13:18:50 +0000

    dodanie testów regresji dla Aggregate

--- src/main/java/com/oss/pages/bigdata/dfe/AggregatePage.java
+++ src/main/java/com/oss/pages/bigdata/dfe/AggregatePage.java
@@ -12,6 +12,8 @@ import com.oss.framework.widgets.table.OldTable;
 
 import io.qameta.allure.Step;
 
+import static com.oss.framework.utils.DelayUtils.waitForPageToLoad;
+
 public class AggregatePage extends BaseDfePage {
 
     private static final Logger log = LoggerFactory.getLogger(AggregatePage.class);
@@ -31,6 +33,16 @@ public class AggregatePage extends BaseDfePage {
     private static final String COLUMN_REQUEST_GENERATION_TIME_LABEL = "Request Generation Time";
     private static final String COLUMN_STATUS_LABEL = "Status";
     private static final String REFRESH_LABEL = "Refresh";
+    private static final String TAB_WIDGET_ID = "card-content_tabsId";
+    private static final String DETAILS_TAB = "Details";
+    private static final String CONFIGURATIONS_TAB = "Configurations";
+    private static final String PROPERTY_PANEL_ID = "detailsId";
+    private static final String NAME_PROPERTY = "Name";
+    private static final String CONFIGURATIONS_TABLE_TAB = "aggregateConfigurationsId";
+    private static final String MEASURES_TAB = "Measures";
+    private static final String MEASURES_TABLE_ID = "aggregateMeasuresId";
+    private static final String USED_IN_TAB = "Used In";
+    private static final String USED_IN_TABLE_TAB = "usedInId";
 
     private AggregatePage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
@@ -115,6 +127,46 @@ public class AggregatePage extends BaseDfePage {
         log.info("Status of last aggregate log in Execution History is {}", statusOfAggregate);
 
         return statusOfAggregate;
+    }
+
+    @Step("Select Details tab")
+    public void selectDetailsTab() {
+        selectTab(TAB_WIDGET_ID, DETAILS_TAB);
+    }
+
+    @Step("Select Configurations tab")
+    public void selectConfigurationsTab() {
+        selectTab(TAB_WIDGET_ID, CONFIGURATIONS_TAB);
+    }
+    @Step("Select Measures tab")
+    public void selectMeasuresTab() {
+        selectTab(TAB_WIDGET_ID, MEASURES_TAB);
+    }
+
+    @Step("Select Used In tab")
+    public void selectUsedTab() {
+        selectTab(TAB_WIDGET_ID, USED_IN_TAB);
+    }
+
+    @Step("Check name in details tab")
+    public String checkNameInPropertyPanel() {
+        waitForPageToLoad(driver, wait);
+        return checkValueInPropertyPanel(PROPERTY_PANEL_ID, NAME_PROPERTY);
+    }
+
+    @Step("Check if Configurations Tab Table is empty")
+    public boolean isConfigurationsTabTableEmpty() {
+        return isTabTableEmpty(CONFIGURATIONS_TABLE_TAB);
+    }
+
+    @Step("Check if Measures Tab Table is empty")
+    public boolean isMeasuresTabTableEmpty() {
+        return isTabTableEmpty(MEASURES_TABLE_ID);
+    }
+
+    @Step("Check if Used In Table is empty")
+    public boolean isUsedInTabTableEmpty() {
+        return isTabTableEmpty(USED_IN_TABLE_TAB);
     }
 
     @Override
---
+++ src/test/java/com/oss/bigdata/dfe/regressiontests/AggregatesRegressionTests.java
@@ -0,0 +1,76 @@
+package com.oss.bigdata.dfe.regressiontests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.oss.BaseTestCase;
+import com.oss.pages.bigdata.dfe.AggregatePage;
+
+import io.qameta.allure.Description;
+
+public class AggregatesRegressionTests extends BaseTestCase{
+
+        private AggregatePage aggregatePage;
+
+        @BeforeMethod
+        public void goToAggregatesView() {
+            aggregatePage = AggregatePage.goToPage(driver, BASIC_URL);
+        }
+
+        @Test(priority = 1, testName = "Check if Aggregates View is opened", description = "Check if Aggregates View is opened")
+        @Description("Check if Aggregates View is opened")
+        public void checkAggregatesView() {
+            Assert.assertEquals(driver.getTitle(), "Aggregates");
+        }
+
+        @Parameters({"aggregateName"})
+        @Test(priority = 2, testName = "Check Aggregate Details Tab", description = "Check Aggregate Details Tab")
+        @Description("Check Aggregate Details Tab")
+        public void checkAggregatesDetailsTab(
+                @Optional("t:SMOKE#AGGRforMonitoring") String aggregateName
+        ) {
+            boolean aggregateExists = aggregatePage.aggregateExistsIntoTable(aggregateName);
+            if (aggregateExists) {
+                aggregatePage.selectFoundAggregate();
+                aggregatePage.selectDetailsTab();
+                Assert.assertEquals(aggregatePage.checkNameInPropertyPanel(), aggregateName);
+            } else {
+                Assert.fail("Cannot find Aggregate " + aggregateName);
+            }
+        }
+
+    @Parameters({"aggregateName"})
+    @Test(priority = 3, testName = "Check Configurations Tab", description = "Check Configurations Tab")
+    @Description("Check Configurations Tab")
+    public void checkConfigurationsTab(
+            @Optional("t:SMOKE#AGGRforMonitoring") String aggregateName
+    ) {
+        boolean aggregateExists = aggregatePage.aggregateExistsIntoTable(aggregateName);
+        if (aggregateExists) {
+            aggregatePage.selectFoundAggregate();
+            aggregatePage.selectConfigurationsTab();
+            Assert.assertFalse(aggregatePage.isConfigurationsTabTableEmpty());
+        } else {
+            Assert.fail("Cannot find Aggregate: " + aggregateName);
+        }
+    }
+
+    @Parameters({"aggregateName"})
+    @Test(priority = 4, testName = "Check Measures Tab", description = "Check Measures Tab")
+    @Description("Check Measures Tab")
+    public void checkMeasuresTab(
+            @Optional("t:SMOKE#AGGRforMonitoring") String aggregateName
+    ) {
+        boolean aggregateExists = aggregatePage.aggregateExistsIntoTable(aggregateName);
+        if (aggregateExists) {
+            aggregatePage.selectFoundAggregate();
+            aggregatePage.selectMeasuresTab();
+            Assert.assertFalse(aggregatePage.isMeasuresTabTableEmpty());
+        } else {
+            Assert.fail("Cannot find Aggregate: " + aggregateName);
+        }
+    }
+}
