8b94a04977fe3a193e79509a270e963b58306092
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2022-07-15 16:49:28 +0200

    ER-OSSTEST-121386 # update Cell4GBulkWizardPage

--- src/main/java/com/oss/pages/radio/Cell4GBulkWizardPage.java
+++ src/main/java/com/oss/pages/radio/Cell4GBulkWizardPage.java
@@ -14,17 +14,13 @@ public class Cell4GBulkWizardPage extends BasePage {
     private static final String USE_FIRST_AVAILABLE_ID = "useFirstAvailableId";
     private static final String USE_SAME_LOCATION_AS_BASE_STATION = "sameLocationAsBS";
     private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_PCI = "pci";
     private static final String COLUMN_CRP = "cellReselectionPriority";
-    private static final String COLUMN_RSI = "rootSequenceIndex";
     private static final String CELLS_AMOUNT = "amountCells";
     private static final String CARRIER = "carrier";
     private static final String BANDWIDTH_UL = "bandwidth_ul";
     private static final String BANDWIDTH_DL = "bandwidth_dl";
     private static final String TAC = "tac";
     private static final String MIMO = "mimo_mode";
-    private static final String PCI = "pci-NUMBER_FIELD";
-    private static final String RSI = "rootSequenceIndex-NUMBER_FIELD";
     private static final String NAME = "name-TEXT_FIELD";
     private static final String COLUMN_LOCAL_CELL_ID = "localCellId";
     private static final String LOCAL_CELL_ID = "localCellId-NUMBER_FIELD";
@@ -32,6 +28,7 @@ public class Cell4GBulkWizardPage extends BasePage {
     private static final String CRP_ID = "cellReselectionPriority-NUMBER_FIELD";
     private static final String WIZARD_ID = "cell-4g-bulk-wizard_prompt-card";
     private static final String CELLS_LIST_ID = "ExtendedList-secondStep";
+    private static final String CELL_TYPE_IOT_ID = "cellTypeIot";
     private final Wizard cell4GBulkWizard;
 
     public Cell4GBulkWizardPage(WebDriver driver) {
@@ -53,6 +50,7 @@ public class Cell4GBulkWizardPage extends BasePage {
     public void createCellBulkWizard(int amountOfCells, String carrier, String[] cellNames, int[] localCellsId, int crp) {
         setCellsAmount(String.valueOf(amountOfCells));
         setCarrier(carrier);
+        setCellTypeIot("STANDALONE_IOT");
         setSameLocation();
         setBandwidthUl("10");
         setBandwidthDl("10");
@@ -72,34 +70,14 @@ public class Cell4GBulkWizardPage extends BasePage {
         clickAccept();
     }
 
-    @Step("Create Cells 4G in Bulk Wizard with default values")
-    public void createCell4GBulkWizardWithDefaultValues(int amountOfCells, String carrier, String[] cellNames, int[] localCellsId, int crp) {
-        setCellsAmount(String.valueOf(amountOfCells));
-        setCarrier(carrier);
-        setSameLocation();
-        setBandwidthUl("10");
-        setBandwidthDl("10");
-        setTac("11000");
-        setMimo("1Tx1Rx");
-        clickNext();
-        setFirstAvailableId();
-        int rowNumber = 1;
-        for (String cellName : cellNames) {
-            Row row = EditableList.createById(driver, wait, CELLS_LIST_ID).getRow(rowNumber - 1);
-            row.setValue(cellName, COLUMN_NAME, NAME);
-            row.setValue(String.valueOf(localCellsId[rowNumber]), COLUMN_LOCAL_CELL_ID, LOCAL_CELL_ID);
-            row.setValue(String.valueOf(crp), COLUMN_CRP, CRP_ID);
-            row.setValue("5", COLUMN_PCI, PCI);
-            row.setValue("10", COLUMN_RSI, RSI);
-            rowNumber++;
-        }
-        DelayUtils.waitForPageToLoad(driver, wait);
-        clickAccept();
-    }
-
     @Step("Set Amount of cells")
     public void setCellsAmount(String cellsAmount) {
         cell4GBulkWizard.setComponentValue(CELLS_AMOUNT, cellsAmount);
+    }
+
+    @Step("Set Cell Type IoT")
+    public void setCellTypeIot(String cellTypeIot) {
+        cell4GBulkWizard.setComponentValue(CELL_TYPE_IOT_ID, cellTypeIot);
     }
 
     @Step("Set Carrier")
