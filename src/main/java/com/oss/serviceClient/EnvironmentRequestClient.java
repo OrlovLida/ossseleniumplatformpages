128241b073b5b9ee4a0ba5741576dd99011f3ebe
Artur Grzelak <artur.grzelak@comarch.com>
2022-09-02 15:49:51 +0200

    FIX OSSRF-12843 # Adding possibility to create request on https:// too #5 - corrections after talking with Krzysztof Pogonowski

--- src/main/java/com/oss/serviceClient/EnvironmentRequestClient.java
+++ src/main/java/com/oss/serviceClient/EnvironmentRequestClient.java
@@ -125,7 +125,7 @@ public class EnvironmentRequestClient {
     }
 
     public RequestSpecification getRecoConfigSpecification() {
-        return servicesClient.getRequestSpecificationByName("reco-config");
+        return servicesClient.getRequestSpecificationByNameUsingBaseURL("reco-config");
     }
 
     public RequestSpecification prepareRequestSpecificationWithoutUri() {
@@ -146,7 +146,7 @@ public class EnvironmentRequestClient {
     }
 
     private String getUrl(Map<String, Object> pathParams, Map<String, Object> queryParams, PlanningContext planningContext,
-            LinkDTO linkDTO) {
+                          LinkDTO linkDTO) {
         UriBuilder uriBuilder = UriBuilder.fromPath(linkDTO.getHref());
         planningContext.applyPlanningContext(uriBuilder);
         uriBuilder = fillUriBuilderWithPathParams(pathParams, uriBuilder);
--- src/test/java/com/oss/reconciliation/ManualMatchingTest.java
+++ src/test/java/com/oss/reconciliation/ManualMatchingTest.java
@@ -67,7 +67,6 @@ public class ManualMatchingTest extends BaseTestCase {
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
         softAssert = new SoftAssert();
         recoConfigClient = RecoConfigClient.getInstance(environmentRequestClient);
-        log.info("Po wzieciu instancji");
         recoConfigClient.createRecoConfig(RECO_CONFIG_BODY, CM_DOMAIN_NAME);
         driver.manage().deleteAllCookies();
     }
