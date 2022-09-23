4d9fa8d3651b3ada45505793bc9e24d6a8bace8c
Anna.Kolczyk <anna.kolczyk@comarch.com>
2022-09-23 09:47:54 +0200

    BIGDATA-7973 - ExternalResourcesRegressionTests

--- src/main/java/com/oss/pages/iaa/bigdata/dfe/externalresource/ExternalResourcesPage.java
+++ src/main/java/com/oss/pages/iaa/bigdata/dfe/externalresource/ExternalResourcesPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.oss.framework.components.inputs.ComponentFactory;
 import com.oss.framework.utils.DelayUtils;
 import com.oss.pages.iaa.bigdata.dfe.BaseDfePage;
 
@@ -14,20 +15,20 @@ public class ExternalResourcesPage extends BaseDfePage {
 
     private static final Logger log = LoggerFactory.getLogger(ExternalResourcesPage.class);
     private static final String TABLE_ID = "ExternalResourcesTableWindowId";
-
     private static final String ADD_NEW_EXTERNAL_RESOURCE_LABEL = "Add New External Resource";
     private static final String EDIT_EXTERNAL_RESOURCE_LABEL = "Edit External Resource";
     private static final String DELETE_EXTERNAL_RESOURCE_LABEL = "Delete External Resource";
     private static final String SEARCH_INPUT_ID = "input_external-resources-listSearchAppId";
-
     private static final String NAME_COLUMN_LABEL = "Name";
     private static final String DELETE_LABEL = "Delete";
+    private static final String EXTERNAL_RESOURCE_PROPERTY_PANEL_ID = "ExternalDetailsAppId";
+    private static final String TYPE_SEARCH_ID = "type";
 
     public ExternalResourcesPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
 
-    @Step("I Open External Resources View")
+    @Step("Open External Resources View")
     public static ExternalResourcesPage goToPage(WebDriver driver, String basicURL) {
         WebDriverWait wait = new WebDriverWait(driver, 45);
 
@@ -35,22 +36,22 @@ public class ExternalResourcesPage extends BaseDfePage {
         return new ExternalResourcesPage(driver, wait);
     }
 
-    @Step("I click add new External Resource")
+    @Step("Click add new External Resource")
     public void clickAddNewExternalResource() {
         clickContextActionAdd();
     }
 
-    @Step("I click edit External Resource")
+    @Step("Click edit External Resource")
     public void clickEditExternalResource() {
         clickContextActionEdit();
     }
 
-    @Step("I click delete External Resource")
+    @Step("Click delete External Resource")
     public void clickDeleteExternalResource() {
         clickContextActionDelete();
     }
 
-    @Step("I check if External Resource: {externalResourceName} exists into the table")
+    @Step("Check if External Resource: {externalResourceName} exists into the table")
     public Boolean externalResourceExistsIntoTable(String externalResourceName) {
         searchFeed(externalResourceName);
         DelayUtils.waitForPageToLoad(driver, wait);
@@ -59,12 +60,33 @@ public class ExternalResourcesPage extends BaseDfePage {
         return numberOfRowsInTable == 1;
     }
 
-    @Step("I select found External Resource")
-    public void selectFoundExternalResource() {
+    @Step("Select first External Resource in table")
+    public void selectFirstExternalResourceInTable() {
         getTable().selectRow(0);
     }
 
-    @Step("I confirm the removal of External Resource")
+    @Step("Check if External Resource Table is empty")
+    public boolean isExternalResourceTableEmpty() {
+        return getTable().hasNoData();
+    }
+
+    @Step("Get first value from column: {columnName}")
+    public String getValueFromColumn(String columnName) {
+        return getTable().getCellValue(0, columnName);
+    }
+
+    @Step("Get value: {propertyName} form External Resource Property Panel")
+    public String getValueFromExternalResourcePropertyPanel(String propertyName) {
+        return getValueFromPropertyPanel(EXTERNAL_RESOURCE_PROPERTY_PANEL_ID, propertyName);
+    }
+
+    @Step("Set type to: {type}")
+    public void setTypeSearch(String type) {
+        ComponentFactory.create(TYPE_SEARCH_ID, driver, wait).setSingleStringValue(type);
+        log.info("Setting type search to: {}", type);
+    }
+
+    @Step("Confirm the removal of External Resource")
     public void confirmDelete() {
         confirmDelete(DELETE_LABEL);
     }
