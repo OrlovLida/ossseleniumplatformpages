a91568167fb863099414de8b9461e0c802be982d
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2022-10-17 15:26:27 +0200

    ER-OSSSELEN-207 # Remove deprecated methods

--- src/main/java/com/oss/pages/nfv/onboardvnf/OnboardVNFWizardPage.java
+++ src/main/java/com/oss/pages/nfv/onboardvnf/OnboardVNFWizardPage.java
@@ -1,14 +1,11 @@
 package com.oss.pages.nfv.onboardvnf;
 
-import com.oss.framework.utils.DelayUtils;
-import com.oss.framework.wizard.Wizard;
-import com.oss.pages.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static com.oss.framework.components.inputs.Input.ComponentType.RADIO_BUTTON;
-import static com.oss.framework.components.inputs.Input.ComponentType.SEARCH_FIELD;
-import static com.oss.framework.components.inputs.Input.ComponentType.TEXT_FIELD;
+import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.wizard.Wizard;
+import com.oss.pages.BasePage;
 
 public class OnboardVNFWizardPage extends BasePage {
 
@@ -33,45 +30,45 @@ public class OnboardVNFWizardPage extends BasePage {
     }
 
     public void selectRadioButton(String label) {
-        wizard.setComponentValue(ONBOARD_RADIO_BUTTONS_COMPONENT_ID, label, RADIO_BUTTON);
+        wizard.setComponentValue(ONBOARD_RADIO_BUTTONS_COMPONENT_ID, label);
         DelayUtils.waitForPageToLoad(driver, wait);
     }
 
     public void searchDownloadNFVO(String name) {
-        wizard.setComponentValue(ONBOARD_DOWNLOAD_NFVO_COMPONENT_ID, name, SEARCH_FIELD);
+        wizard.setComponentValue(ONBOARD_DOWNLOAD_NFVO_COMPONENT_ID, name);
         DelayUtils.waitForPageToLoad(driver, wait);
     }
 
     public String getSelectedDownloadNFVO() {
-        return wizard.getComponent(ONBOARD_DOWNLOAD_NFVO_COMPONENT_ID, SEARCH_FIELD).getStringValue();
+        return wizard.getComponent(ONBOARD_DOWNLOAD_NFVO_COMPONENT_ID).getStringValue();
     }
 
     public String getSelectedDownloadVNFPKG() {
-        return wizard.getComponent(ONBOARD_DOWNLOAD_VNFPKG_COMPONENT_ID, SEARCH_FIELD).getStringValue();
+        return wizard.getComponent(ONBOARD_DOWNLOAD_VNFPKG_COMPONENT_ID).getStringValue();
     }
 
     public void searchNFVO(String name) {
-        wizard.setComponentValue(ONBOARD_NFVO_COMPONENT_ID, name, SEARCH_FIELD);
+        wizard.setComponentValue(ONBOARD_NFVO_COMPONENT_ID, name);
         DelayUtils.waitForPageToLoad(driver, wait);
     }
 
     public String getSelectedNFVO() {
-        return wizard.getComponent(ONBOARD_NFVO_COMPONENT_ID, SEARCH_FIELD).getStringValue();
+        return wizard.getComponent(ONBOARD_NFVO_COMPONENT_ID).getStringValue();
     }
 
     public String getSelectedMarketplace() {
-        return wizard.getComponent(ONBOARD_MARKETPLACE_COMPONENT_ID, SEARCH_FIELD).getStringValue();
+        return wizard.getComponent(ONBOARD_MARKETPLACE_COMPONENT_ID).getStringValue();
     }
 
     public String getSelectedVIM() {
-        return wizard.getComponent(ONBOARD_VIM_SEARCH_COMPONENT_ID, SEARCH_FIELD).getStringValue();
+        return wizard.getComponent(ONBOARD_VIM_SEARCH_COMPONENT_ID).getStringValue();
     }
 
     public void fillDefaultRemoteFolder(String value) {
-        wizard.setComponentValue(ONBOARD_DEFAULT_REMOTE_FOLDER_COMPONENT_ID, value, TEXT_FIELD);
+        wizard.setComponentValue(ONBOARD_DEFAULT_REMOTE_FOLDER_COMPONENT_ID, value);
     }
 
     public String getDefaultRemoteFolder() {
-        return wizard.getComponent(ONBOARD_DEFAULT_REMOTE_FOLDER_COMPONENT_ID, TEXT_FIELD).getStringValue();
+        return wizard.getComponent(ONBOARD_DEFAULT_REMOTE_FOLDER_COMPONENT_ID).getStringValue();
     }
 }
