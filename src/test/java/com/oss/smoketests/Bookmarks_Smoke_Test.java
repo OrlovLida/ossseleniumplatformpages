19a86ed5f6aee85cfd03173c9713f8e708f5fec5
Kamil Szota <kamil.szota@comarch.com>
2022-11-21 17:30:11 +0100

    OSSSELEN-259 # Fixed NewBookmarksPage class # Fixed Bookmarks_Smoke_Test class

--- src/test/java/com/oss/smoketests/Bookmarks_Smoke_Test.java
+++ src/test/java/com/oss/smoketests/Bookmarks_Smoke_Test.java
@@ -2,7 +2,6 @@ package com.oss.smoketests;
 
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +17,6 @@ import com.oss.framework.navigation.toolsmanager.ToolsManagerWindow;
 import com.oss.framework.utils.DelayUtils;
 import com.oss.framework.widgets.advancedsearch.AdvancedSearchWidget;
 import com.oss.framework.widgets.tabs.TabsWidget;
-import com.oss.pages.platform.bookmarksanddashboards.BaseBookmarkAndDashboardPage;
-import com.oss.pages.platform.bookmarksanddashboards.CategoryWizardPage;
 import com.oss.pages.platform.bookmarksanddashboards.bookmarks.BookmarkWizardPage;
 import com.oss.pages.platform.bookmarksanddashboards.bookmarks.NewBookmarksPage;
 import com.oss.pages.transport.NetworkViewPage;
@@ -27,16 +24,13 @@ import com.oss.untils.FakeGenerator;
 
 import io.qameta.allure.Description;
 
-import static com.oss.BaseTestCase.BASIC_URL;
 import static com.oss.pages.platform.bookmarksanddashboards.bookmarks.NewBookmarksPage.MANAGEMENT_VIEW_CONTAINER_TABSCARD;
 import static com.oss.web.NewBookmarksTest.BOOKMARK_PROPERTIES;
 import static com.oss.web.NewBookmarksTest.BUTTON_SAVE_BOOKMARK;
 
-public class BookmarksSmokeTest extends BaseTestCase{
-    private static final Logger LOGGER = LoggerFactory.getLogger(BookmarksSmokeTest.class);
+public class Bookmarks_Smoke_Test extends BaseTestCase {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Bookmarks_Smoke_Test.class);
     NewBookmarksPage bookmarksPage;
-    BaseBookmarkAndDashboardPage baseBookmarkAndDashboardPage;
-    private NetworkViewPage networkViewPage;
     private AdvancedSearchWidget advancedSearchWidget;
     private static final String ADVANCED_SEARCH_WIDGET_ID = "advancedSearch";
     private static final int INDEX_OF_FIRST_ELEMENT_IN_TABLE = 0;
@@ -61,26 +55,26 @@ public class BookmarksSmokeTest extends BaseTestCase{
         checkGlobalNotificationContainer(BOOKMARKS_PAGE);
     }
 
-    @Test(priority = 2, description = "Create Category")
+    @Test(priority = 2, description = "Create Category", dependsOnMethods = {"openBookmarksView"})
     @Description("Create Category")
     public void createCategory() {
         bookmarksPage.createCategory(CATEGORY_NAME, DESCRIPTION_CATEGORY);
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
         checkErrorPage(BOOKMARKS_PAGE);
         checkGlobalNotificationContainer(BOOKMARKS_PAGE);
-        Assertions.assertThat(bookmarksPage.isObjectPresent(CATEGORY_NAME)).isTrue();
+        Assert.assertTrue(bookmarksPage.isObjectPresent(CATEGORY_NAME));
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
-        Assertions.assertThat(bookmarksPage.getDescription(CATEGORY_NAME)).isEqualTo(DESCRIPTION_CATEGORY);
+        Assert.assertEquals(bookmarksPage.getDescription(CATEGORY_NAME), DESCRIPTION_CATEGORY);
     }
 
-    @Test(priority = 3, description = "Open Network View and Add Location To View ")
+    @Test(priority = 3, description = "Open Network View and Add Location To View ", dependsOnMethods = {"createCategory"})
     @Description("Open Network View and Add Location To View ")
     public void openNetworkViewAndAddLocationToView() {
         TabsWidget tabs = TabsWidget.createById(driver, webDriverWait, MANAGEMENT_VIEW_CONTAINER_TABSCARD);
         tabs.selectTabById(TAB_TOOLS_ID);
         ToolsManagerWindow toolsManagerWindow = ToolsManagerWindow.create(driver, webDriverWait);
         toolsManagerWindow.openApplication(CATEGORY, VIEW_NAME);
-        networkViewPage = new NetworkViewPage(driver);
+        NetworkViewPage networkViewPage = new NetworkViewPage(driver);
         networkViewPage.useContextAction(NetworkViewPage.ADD_TO_VIEW_ACTION, LOCATION_ACTION);
         advancedSearchWidget = AdvancedSearchWidget.createById(driver, webDriverWait, ADVANCED_SEARCH_WIDGET_ID);
         advancedSearchWidget.getTableComponent().clickRow(INDEX_OF_FIRST_ELEMENT_IN_TABLE);
@@ -88,9 +82,10 @@ public class BookmarksSmokeTest extends BaseTestCase{
         checkErrorPage(NETWORK_PAGE);
         checkGlobalNotificationContainer(NETWORK_PAGE);
     }
-    @Test(priority = 4, description = "Open Network View, add Location to view and Save Bookmark")
-    @Description("Open Network View, add Location to view and Save Bookmark")
-    public void saveBookmark(){
+
+    @Test(priority = 4, description = "Save new Bookmark on Network View", dependsOnMethods = {"openNetworkViewAndAddLocationToView"})
+    @Description("Save new Bookmark on Network View")
+    public void saveBookmark() {
         ButtonPanel.create(driver, webDriverWait).clickButton(BUTTON_SAVE_BOOKMARK);
         BookmarkWizardPage bookmarkWizardPage = new BookmarkWizardPage(driver, webDriverWait);
         bookmarkWizardPage.setName(BOOKMARK_PROPERTIES);
@@ -102,19 +97,19 @@ public class BookmarksSmokeTest extends BaseTestCase{
         checkGlobalNotificationContainer(NETWORK_PAGE);
     }
 
-    @Test(priority = 5, description = "Open Bookmarks View and  check Bookmark on Network View")
+    @Test(priority = 5, description = "Open Bookmarks View and  check Bookmark on Network View", dependsOnMethods = {"saveBookmark"})
     @Description("Open Bookmarks View and check Bookmark on Network View")
-    public void checkBookmarkOnNetworkView(){
+    public void checkBookmarkOnNetworkView() {
         bookmarksPage = NewBookmarksPage.goToBookmarksPage(driver, webDriverWait, BASIC_URL);
         bookmarksPage.expandCategory(CATEGORY_NAME);
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
-        Assertions.assertThat(bookmarksPage.isObjectPresent(BOOKMARK_PROPERTIES)).isTrue();
+        Assert.assertTrue(bookmarksPage.isObjectPresent(BOOKMARK_PROPERTIES));
         bookmarksPage.openBookmark(BOOKMARK_PROPERTIES);
         checkErrorPage(NETWORK_PAGE);
         checkGlobalNotificationContainer(NETWORK_PAGE);
     }
 
-    @Test(priority = 6, description = "Edit Bookmark on Bookmarks View")
+    @Test(priority = 6, description = "Edit Bookmark on Bookmarks View", dependsOnMethods = {"saveBookmark"})
     @Description("Edit Bookmark on Bookmarks View")
     public void editBookmarkOnBookmarksView() {
         bookmarksPage = NewBookmarksPage.goToBookmarksPage(driver, webDriverWait, BASIC_URL);
@@ -123,22 +118,22 @@ public class BookmarksSmokeTest extends BaseTestCase{
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
         bookmarksPage.editBookmark(BOOKMARK_PROPERTIES, UPDATED_BOOKMARK_NAME, UPDATED_BOOKMARK_DESCRIPTION);
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
-        Assertions.assertThat(bookmarksPage.isObjectPresent(UPDATED_BOOKMARK_NAME)).isTrue();
+        Assert.assertTrue(bookmarksPage.isObjectPresent(UPDATED_BOOKMARK_NAME));
         checkErrorPage(BOOKMARKS_PAGE);
         checkGlobalNotificationContainer(BOOKMARKS_PAGE);
     }
 
-    @Test(priority = 7, description = "Delete Bookmark on Bookmarks View")
+    @Test(priority = 7, description = "Delete Bookmark on Bookmarks View", dependsOnMethods = {"saveBookmark"})
     @Description("Delete Bookmark on Bookmarks View")
-    public void deleteBookmarkOnBookmarksView(){
+    public void deleteBookmarkOnBookmarksView() {
         bookmarksPage.deleteBookmark(UPDATED_BOOKMARK_NAME);
         DelayUtils.sleep();
-        Assertions.assertThat(bookmarksPage.isObjectPresent(UPDATED_BOOKMARK_NAME)).isFalse();
+        Assert.assertFalse(bookmarksPage.isObjectPresent(UPDATED_BOOKMARK_NAME));
         checkErrorPage(BOOKMARKS_PAGE);
         checkGlobalNotificationContainer(BOOKMARKS_PAGE);
     }
 
-    @Test(priority = 8, description = "Delete Category on Bookmarks View")
+    @Test(priority = 8, description = "Delete Category on Bookmarks View", dependsOnMethods = {"createCategory"})
     @Description("Delete Category on Bookmarks View")
     public void deleteCategoryOnBookmarksView() {
         bookmarksPage.deleteCategory(CATEGORY_NAME);
@@ -150,18 +145,11 @@ public class BookmarksSmokeTest extends BaseTestCase{
         ErrorCard errorCard = ErrorCard.create(driver, webDriverWait);
         if (errorCard.isErrorPagePresent()) {
             ErrorCard.ErrorInformation errorInformation = errorCard.getErrorInformation();
-            if (errorInformation.getErrorText().endsWith("403")) {
-                LOGGER.warn("Access denied for {} page.", viewName);
-                SystemMessageContainer.create(driver, webDriverWait).close();
-                return;
-            } else {
-                LOGGER.error(errorInformation.getErrorText());
-                LOGGER.error(errorInformation.getErrorDescription());
-                LOGGER.error(errorInformation.getErrorMessage());
-                Assert.fail(String.format("Error Page is shown on %s page.", viewName));
-            }
+            LOGGER.error(errorInformation.getErrorText());
+            LOGGER.error(errorInformation.getErrorDescription());
+            LOGGER.error(errorInformation.getErrorMessage());
+            Assert.fail(String.format("Error Page is shown on %s page.", viewName));
         }
-        checkSystemMessage();
     }
 
     private void checkSystemMessage() {
