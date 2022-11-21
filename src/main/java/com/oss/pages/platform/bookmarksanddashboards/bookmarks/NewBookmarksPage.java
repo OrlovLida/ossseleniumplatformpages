2ec10f858dfddba9c6916fef147bef6586007634
Kamil Szota <kamil.szota@comarch.com>
2022-11-21 17:30:11 +0100

    OSSSELEN-259 + Added editBookmark and deleteBookmark methods to NewBookmarksPage class + Added BookmarksSmokeTest Test

--- src/main/java/com/oss/pages/platform/bookmarksanddashboards/bookmarks/NewBookmarksPage.java
+++ src/main/java/com/oss/pages/platform/bookmarksanddashboards/bookmarks/NewBookmarksPage.java
@@ -1,26 +1,35 @@
 package com.oss.pages.platform.bookmarksanddashboards.bookmarks;
 
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.oss.framework.components.inputs.Button;
 import com.oss.framework.components.prompts.Popup;
+import com.oss.framework.utils.DelayUtils;
 import com.oss.framework.widgets.tabs.TabsWidget;
 import com.oss.pages.platform.bookmarksanddashboards.BaseBookmarkAndDashboardPage;
 import com.oss.pages.platform.bookmarksanddashboards.CategoryWizardPage;
 
+import static com.oss.framework.components.contextactions.ActionsContainer.ASSIGN_GROUP_ID;
+
 public class NewBookmarksPage extends BaseBookmarkAndDashboardPage {
 
     public static final String ACTION_CATEGORY = "action_createCategoryBookmark";
+    public static final String ACTION_SHARE_CATEGORY = "ShareFolderAction";
     public static final String MANAGEMENT_VIEW_CONTAINER_TABSCARD = "management-view__container__tabscard";
 
     public static final String ACTION_EDIT_BOOKMARK_FOLDER = "action_editBookmarkFolder";
     public static final String ACTION_DELETE_BOOKMARK_FOLDER = "action_deleteBookmarkFolder";
+    public static final String ACTION_DELETE_BOOKMARK = "action_deleteBookmark";
+    public static final String EDIT_BOOKMARK_ACTION = "action_editBookmark";
     public static final String DELETE = "Delete";
 
     public static final String ID = "id";
     public static final String TABLE_ID = "tab_bookmark_manager_widget";
     private static final String TAB_BOOKMARKS_ID = "tab_bookmarks";
     private static final String SAVE_BOOKMARK_BUTTON_ID = "saveBookmarkBtn";
+    private static final String ACCEPT_BUTTON_ID = "wizard-submit-button-share-widget-id";
 
     protected NewBookmarksPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
@@ -56,6 +65,24 @@ public class NewBookmarksPage extends BaseBookmarkAndDashboardPage {
         callActionById(ACTION_DELETE_BOOKMARK_FOLDER);
         Popup popup = Popup.create(driver, wait);
         popup.clickButtonByLabel(DELETE);
+    }
+
+    public void editBookmark(String bookmarkName, String newBookmarkName, String newDescription){
+        BookmarkWizardPage bookmarkWizardPage = new BookmarkWizardPage(driver, wait);
+        selectObject(bookmarkName);
+        callActionById(EDIT_BOOKMARK_ACTION);
+        bookmarkWizardPage.setName(newBookmarkName);
+        bookmarkWizardPage.setDescription(newDescription);
+        bookmarkWizardPage.clickSave();
+    }
+
+    public void deleteBookmark(String bookmarkName){
+        selectObject(bookmarkName);
+        callActionById(ACTION_DELETE_BOOKMARK);
+        Popup popup = Popup.create(driver, wait);
+        popup.clickButtonByLabel(DELETE);
+        Assertions.assertThat(isObjectPresent(bookmarkName)).isFalse();
+        DelayUtils.sleep();
     }
 
     public void openBookmark(String nameBookmark) {
