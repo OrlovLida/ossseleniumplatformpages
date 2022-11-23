9244efc8cc25f0a808713c0d0ae6284be3541e4c
Kinga.Balcar-Mazur <kinga.balcar-mazur@comarch.com>
2022-11-23 10:21:58 +0100

    rozbudowanie suity testowej dla CSDI o moduł IM

--- src/main/java/com/oss/pages/iaa/servicedesk/infomanagement/NotificationPreviewPage.java
+++ src/main/java/com/oss/pages/iaa/servicedesk/infomanagement/NotificationPreviewPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.oss.framework.components.prompts.ConfirmationBox;
 import com.oss.framework.iaa.widgets.components.NotificationPreview;
 import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.widgets.table.TableWidget;
 import com.oss.pages.iaa.servicedesk.BaseSDPage;
 import com.oss.pages.iaa.servicedesk.issue.wizard.SDWizardPage;
 
@@ -20,6 +21,8 @@ public class NotificationPreviewPage extends BaseSDPage {
     private static final String PROCESS_NOTIFICATION_ID = "process";
     private static final String PROCESS_NOTIFICATION_PROMPT_ID = "notification-process-wizard-view_prompt-card";
     private static final String CONFIRM_PROCESS_NOTIFICATION_BUTTON_LABEL = "Process Notification";
+    private static final String CREATE_MESSAGE_WIZARD_ID = "notification-wizard_prompt-card";
+    private static final String REPLY_ID = "reply";
 
     public NotificationPreviewPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
@@ -63,5 +66,12 @@ public class NotificationPreviewPage extends BaseSDPage {
         ConfirmationBox.create(driver, wait).clickButtonByLabel(CONFIRM_PROCESS_NOTIFICATION_BUTTON_LABEL);
         log.info("Clicking confirm Process Notification");
         DelayUtils.waitForPageToLoad(driver, wait);
+    }
+
+    @Step("Click Reply button")
+    public SDWizardPage clickReply() {
+        notificationPreview.getActionsContainer().callActionById(REPLY_ID);
+        log.info("Clicking Reply button");
+        return new SDWizardPage(driver, wait, CREATE_MESSAGE_WIZARD_ID);
     }
 }
