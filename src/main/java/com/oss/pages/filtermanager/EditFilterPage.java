8ddea1d0658e37a9256d8f6babf335b2391b416b
Faustyna.Szczepanik <faustyna.szczepanik@comarch.com>
2022-12-07 08:39:03 +0100

    ER OSSWEB-21388

--- src/main/java/com/oss/pages/filtermanager/EditFilterPage.java
+++ src/main/java/com/oss/pages/filtermanager/EditFilterPage.java
@@ -8,14 +8,15 @@ import com.oss.framework.utils.DragAndDrop;
 import com.oss.framework.wizard.Wizard;
 
 import io.qameta.allure.Step;
+import static com.oss.framework.components.inputs.Input.ComponentType.TEXT_FIELD;
 
 public class EditFilterPage extends FilterManagerPage {
 
-    private static final String TO_DROP_XPATH = "//div[@data-rbd-droppable-id ='filterManager_wizard_rel_enabled']";
-    private static final String TO_TAKE_XPATH = "//div[@class='btn-drag']";
     private static final String ACCEPT_BUTTON_ID = "wizard-submit-button-webFilter_wizard_filter_widget";
     private static final String WIZARD_ID = "webFilter_wizard_filter_widget";
-    private Wizard folderWizard = Wizard.createByComponentId(driver, wait, WIZARD_ID);
+    private static final String NAME_TEXT_FIELD_ID = "filterManager_wizard_def_name";
+    private static final String DESCRIPTION_TEXT_FIELD_ID = "filterManager_wizard_def_desc";
+    private final Wizard filterWizard = Wizard.createByComponentId(driver, wait, WIZARD_ID);
 
     public EditFilterPage(WebDriver driver) {
         super(driver);
@@ -28,14 +29,26 @@ public class EditFilterPage extends FilterManagerPage {
         DraggableList enabledFolders = DraggableList.create(driver, wait, "Enabled folders");
         DragAndDrop.DraggableElement source = availableFolders.getDraggableElement(folderName);
         enabledFolders.drop(source);
-        //     DragAndDrop.dragAndDrop(TO_TAKE_XPATH, TO_DROP_XPATH, driver);
         return this;
     }
+
+    @Step("Change Name of filter")
+    public EditFilterPage changeName(String folderName) {
+        filterWizard.getComponent(NAME_TEXT_FIELD_ID, TEXT_FIELD).setSingleStringValue(folderName);
+        return this;
+    }
+
+    @Step("Change Description of filter")
+    public EditFilterPage changeDescription(String folderDescription) {
+        filterWizard.getComponent(DESCRIPTION_TEXT_FIELD_ID, TEXT_FIELD).setSingleStringValue(folderDescription);
+        return this;
+    }
+
 
     @Step("Click on Accept and close the wizard")
     public FilterManagerPage clickAccept() {
         DelayUtils.waitForPageToLoad(driver, wait);
-        folderWizard.clickButtonById(ACCEPT_BUTTON_ID);
+        filterWizard.clickButtonById(ACCEPT_BUTTON_ID);
         return new FilterManagerPage(driver);
     }
 }
