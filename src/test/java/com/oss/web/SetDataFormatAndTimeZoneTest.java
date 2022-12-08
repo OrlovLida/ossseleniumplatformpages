bb8aacdeb5d375aba27da980bf8ca74656ccd546
NBGZARANEK <gabriela.zaranek@comarch.pl>
2022-12-08 16:29:30 +0100

    ER OSSWEB-20887 # small fix in SetDataFormatAndTimeZoneTest

--- src/test/java/com/oss/web/SetDataFormatAndTimeZoneTest.java
+++ src/test/java/com/oss/web/SetDataFormatAndTimeZoneTest.java
@@ -1,16 +1,18 @@
 package com.oss.web;
 
-import com.oss.framework.utils.DelayUtils;
-import com.oss.framework.widgets.propertypanel.PropertyPanel;
-import com.oss.pages.platform.NewInventoryViewPage;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.oss.BaseTestCase;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.widgets.propertypanel.PropertyPanel;
+import com.oss.pages.platform.NewInventoryViewPage;
 
 /**
  * @author Faustyna Szczepanik
@@ -31,6 +33,10 @@ public class SetDataFormatAndTimeZoneTest extends BaseTestCase {
     private static final String PASSWORD_2 = "oss";
     private final static String USER1 = "webseleniumtests";
     private static final String PASSWORD_1 = "Webtests123!";
+    private static final String AUSTRALIA_SYDNEY_ZONE = "Australia/Sydney";
+    private static final String EUROPE_PARIS_ZONE = "Europe/Paris";
+    private static final String TIME_ZONE_EXCEPTION = "Value of Data (Created) attribute is incorrect. Proper value is ";
+    private static final String DATA_TIME_FORMAT_EXCEPTION = "Data Time Format is incorrect. Proper format should be";
     private NewInventoryViewPage inventoryViewPage;
 
     private static final DateTimeFormatter DFT1 = DateTimeFormatter.ofPattern(DATE_FORMAT1);
@@ -44,47 +50,57 @@ public class SetDataFormatAndTimeZoneTest extends BaseTestCase {
 
     @Test(priority = 1)
     public void changeDataFormat() {
-
         homePage.chooseDataFormat(PRESENT_FORMAT, DFT1);
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
         PropertyPanel propertyPanel = inventoryViewPage.getPropertyPanel(ROW_ID, PROPERTY_PANEL_ID);
-        propertyPanel.enableAttributeByLabel(DATE_CREATED_LABEL, AUDIT_INFORMATION_LABEL, CREATED_LABEL);
+        if (!propertyPanel.getVisibleAttributes().contains(DATE_CREATED_ID)) {
+            propertyPanel.enableAttributeByLabel(DATE_CREATED_LABEL, AUDIT_INFORMATION_LABEL, CREATED_LABEL);
+        }
         String dateCreatedAfterChangeFormatValue = propertyPanel.getPropertyValue(DATE_CREATED_ID);
-        Assert.assertTrue(checkDateFormat(DFT1, dateCreatedAfterChangeFormatValue));
+        Assert.assertTrue(checkDateFormat(DFT1, dateCreatedAfterChangeFormatValue), DATA_TIME_FORMAT_EXCEPTION + DFT1);
     }
 
     @Test(priority = 2)
     public void checkIfDataFormatIsSavedForUser() {
         inventoryViewPage.changeUser(USER2, PASSWORD_2);
         inventoryViewPage = NewInventoryViewPage.goToInventoryViewPage(driver, BASIC_URL, LOCATION_TYPE);
-        PropertyPanel propertyPanel = inventoryViewPage.getPropertyPanel(ROW_ID, PROPERTY_PANEL_ID);
-        String dateCreatedValue = propertyPanel.getPropertyValue(DATE_CREATED_ID);
-        Assert.assertTrue(checkDateFormat(PRESENT_FORMAT, dateCreatedValue));
+        String dateCreatedValue = getCreatedObjectDataTime();
+        Assert.assertTrue(checkDateFormat(PRESENT_FORMAT, dateCreatedValue), DATA_TIME_FORMAT_EXCEPTION + PRESENT_FORMAT);
 
         inventoryViewPage.changeUser(USER1, PASSWORD_1);
         inventoryViewPage = NewInventoryViewPage.goToInventoryViewPage(driver, BASIC_URL, LOCATION_TYPE);
-        PropertyPanel propertyPanel1 = inventoryViewPage.getPropertyPanel(ROW_ID, PROPERTY_PANEL_ID);
-        String dateCreatedValueU1 = propertyPanel1.getPropertyValue(DATE_CREATED_ID);
-        Assert.assertTrue(checkDateFormat(DFT1, dateCreatedValueU1));
+        String dateCreatedValueU1 = getCreatedObjectDataTime();
+        Assert.assertTrue(checkDateFormat(DFT1, dateCreatedValueU1), DATA_TIME_FORMAT_EXCEPTION + DFT1);
     }
 
     @Test(priority = 3)
     public void changeTimeZone() {
-        PropertyPanel propertyPanel = inventoryViewPage.getPropertyPanel(ROW_ID, PROPERTY_PANEL_ID);
-        String dateCreatedValueInDefaultTimeZone = propertyPanel.getPropertyValue(DATE_CREATED_ID);
+        homePage.chooseDataFormat(PRESENT_FORMAT, DFT1);
+        String dateCreatedValueInDefaultTimeZone = getCreatedObjectDataTime();
         LocalDateTime dateCreatedValue = LocalDateTime.parse(dateCreatedValueInDefaultTimeZone, DFT1);
-        LocalDateTime shiftedDateTimeAsiaTokio = dateCreatedValue.plusHours(8);
+        LocalDateTime shiftedDateTimeAsiaTokio = getAustraliaDataTime(dateCreatedValue);
         homePage.disableAutoTimeZone();
         homePage.chooseTimeZone(TIME_ZONE_1);
-        PropertyPanel propertyPanel1 = inventoryViewPage.getPropertyPanel(ROW_ID, PROPERTY_PANEL_ID);
-        String dateCreatedAfterChangeZone = propertyPanel1.getPropertyValue(DATE_CREATED_ID);
+        String dateCreatedAfterChangeZone = getCreatedObjectDataTime();
         LocalDateTime dateCreatedValueAfterChangeZone = LocalDateTime.parse(dateCreatedAfterChangeZone, DFT1);
-        Assert.assertEquals(shiftedDateTimeAsiaTokio, dateCreatedValueAfterChangeZone);
+        Assert.assertEquals(dateCreatedValueAfterChangeZone, shiftedDateTimeAsiaTokio, TIME_ZONE_EXCEPTION + shiftedDateTimeAsiaTokio);
 
         homePage.enableAutoTimeZone();
-        PropertyPanel propertyPanel2 = inventoryViewPage.getPropertyPanel(ROW_ID, PROPERTY_PANEL_ID);
-        String dateCreatedAfterEnableAutoTimeZone = propertyPanel2.getPropertyValue(DATE_CREATED_ID);
+        String dateCreatedAfterEnableAutoTimeZone = getCreatedObjectDataTime();
         Assert.assertEquals(dateCreatedValueInDefaultTimeZone, dateCreatedAfterEnableAutoTimeZone);
+    }
+
+    private static LocalDateTime getAustraliaDataTime(LocalDateTime dateCreatedValue) {
+        ZoneId zoneId = ZoneId.of(AUSTRALIA_SYDNEY_ZONE);
+        ZonedDateTime europe = ZonedDateTime.of(dateCreatedValue, ZoneId.of(EUROPE_PARIS_ZONE));
+        ZonedDateTime australia = europe.withZoneSameInstant(zoneId);
+        return australia.toLocalDateTime();
+
+    }
+
+    private String getCreatedObjectDataTime() {
+        PropertyPanel propertyPanel = inventoryViewPage.getPropertyPanel(ROW_ID, PROPERTY_PANEL_ID);
+        return propertyPanel.getPropertyValue(DATE_CREATED_ID);
     }
 
     private boolean checkDateFormat(DateTimeFormatter dft, String data) {
@@ -92,8 +108,9 @@ public class SetDataFormatAndTimeZoneTest extends BaseTestCase {
             dft.parse(data);
             return true;
         } catch (Exception ex) {
+            return false;
         }
-        return false;
+
     }
 }
 
