e64fb1866ac12aad6f14646b170d749cf8a60dfc
Faustyna.Szczepanik <faustyna.szczepanik@comarch.com>
2023-02-14 14:41:10 +0100

    ER OSSWEB-22392
    Add tests for Autorefresh HV

--- src/main/java/com/oss/pages/physical/ChassisWizardPage.java
+++ src/main/java/com/oss/pages/physical/ChassisWizardPage.java
@@ -1,22 +1,31 @@
 package com.oss.pages.physical;
 
+import org.openqa.selenium.WebDriver;
+
 import com.oss.framework.utils.DelayUtils;
 import com.oss.framework.wizard.Wizard;
 import com.oss.pages.BasePage;
+
 import io.qameta.allure.Step;
-import org.openqa.selenium.WebDriver;
 
 public class ChassisWizardPage extends BasePage {
 
-    public static final String WIZARD_ID = "chassis_create_wizard_view";
+    public static final String CHASSIS_CREATE_WIZARD_ID = "chassis_create_wizard_view";
+    public static final String CHASSIS_UPDATE_WIZARD_ID = "chassis_update_wizard_view";
     private static final String MODEL_ID = "model";
     private static final String CHASSIS_NAME_ID = "name";
+    private static final String DESCRIPTION_ID = "description";
 
-    public ChassisWizardPage(WebDriver driver) { super (driver);}
+    public ChassisWizardPage(WebDriver driver) {
+        super(driver);
+    }
 
     private Wizard getWizard() {
         DelayUtils.waitForPageToLoad(driver, wait);
-        return Wizard.createByComponentId(driver, wait, WIZARD_ID);
+        if (driver.getPageSource().contains(CHASSIS_CREATE_WIZARD_ID)) {
+            return Wizard.createByComponentId(driver, wait, CHASSIS_CREATE_WIZARD_ID);
+        }
+        return Wizard.createByComponentId(driver, wait, CHASSIS_UPDATE_WIZARD_ID);
     }
 
     @Step("Set Model")
@@ -27,6 +36,11 @@ public class ChassisWizardPage extends BasePage {
     @Step("Set Name")
     public void setName(String name) {
         getWizard().setComponentValue(CHASSIS_NAME_ID, name);
+    }
+
+    @Step("Set Description")
+    public void setDescription(String description) {
+        getWizard().setComponentValue(DESCRIPTION_ID, description);
     }
 
     @Step("Click Accept button")
