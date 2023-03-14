97c78e7c812479506708ec04a0654bbe8cefad46
Aleksandra Pieszka <aleksandra.pieszka@comarch.com>
2023-03-14 09:58:44 +0000

    Dodanie stepa z wybieraniem folderu przy tworzeniu WAMV i WHAV

--- src/test/java/com/oss/iaa/faultmanagement/CreateAlarmListTest.java
+++ src/test/java/com/oss/iaa/faultmanagement/CreateAlarmListTest.java
@@ -1,19 +1,17 @@
 package com.oss.iaa.faultmanagement;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import com.oss.BaseTestCase;
+import com.oss.pages.iaa.faultmanagement.FMCreateWAMVPage;
+import com.oss.pages.iaa.faultmanagement.FMSMDashboardPage;
+import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.oss.BaseTestCase;
-import com.oss.pages.iaa.faultmanagement.FMCreateWAMVPage;
-import com.oss.pages.iaa.faultmanagement.FMSMDashboardPage;
-
-import io.qameta.allure.Description;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author APodolska
@@ -33,24 +31,26 @@ public class CreateAlarmListTest extends BaseTestCase {
         fmsmDashboardPage = FMSMDashboardPage.goToPage(driver, BASIC_URL, chosenDashboard);
     }
 
-    @Parameters({"name", "description", "folderName", "rowNumber"})
+    @Parameters({"name", "description", "folderName", "rowNumber", "bookmarkCategory"})
     @Test(priority = 1, testName = "Create new alarm management view", description = "Set name, description, folder and filter")
     @Description("I verify if it is possible to create Web Alarm Management View")
     public void createNewWAMVandDeleteIt(
             @Optional("Selenium_test_alarm_list") String name,
             @Optional("Selenium test description") String description,
+            @Optional("Selenium_fm_folder") String bookmarkCategory,
             @Optional("Selenium_test_folder") String folderName,
             @Optional("0") int rowNumber
     ) {
-            fmWAMVPage = fmsmDashboardPage.clickCreateNewAlarmList(ALARM_MANAGEMENT_VIEW_ID);
-            fmWAMVPage.setName(name + '_' + date.replace(":", "_"));
-            fmWAMVPage.setDescription(description);
-            fmWAMVPage.dragAndDropFilterByName(folderName);
-            fmWAMVPage.selectFilterFromList(rowNumber);
-            fmWAMVPage.clickAcceptButton();
-            Assert.assertTrue(fmsmDashboardPage.checkVisibility(ALARM_MANAGEMENT_VIEW_ID, name + '_' + date.replace(":", "_")));
-            fmsmDashboardPage.searchInView(ALARM_MANAGEMENT_VIEW_ID, name + '_' + date.replace(":", "_"));
-            fmsmDashboardPage.deleteFromView(ALARM_MANAGEMENT_VIEW_ID, 0);
-            Assert.assertFalse(fmsmDashboardPage.checkVisibility(ALARM_MANAGEMENT_VIEW_ID, name + '_' + date.replace(":", "_")));
+        fmWAMVPage = fmsmDashboardPage.clickCreateNewAlarmList(ALARM_MANAGEMENT_VIEW_ID);
+        fmWAMVPage.setName(name + '_' + date.replace(":", "_"));
+        fmWAMVPage.setDescription(description);
+        fmWAMVPage.chooseBookmarkCategory(bookmarkCategory);
+        fmWAMVPage.dragAndDropFilterByName(folderName);
+        fmWAMVPage.selectFilterFromList(rowNumber);
+        fmWAMVPage.clickAcceptButton();
+        Assert.assertTrue(fmsmDashboardPage.checkVisibility(ALARM_MANAGEMENT_VIEW_ID, name + '_' + date.replace(":", "_")));
+        fmsmDashboardPage.searchInView(ALARM_MANAGEMENT_VIEW_ID, name + '_' + date.replace(":", "_"));
+        fmsmDashboardPage.deleteFromView(ALARM_MANAGEMENT_VIEW_ID, 0);
+        Assert.assertFalse(fmsmDashboardPage.checkVisibility(ALARM_MANAGEMENT_VIEW_ID, name + '_' + date.replace(":", "_")));
     }
 }
--- src/test/java/com/oss/iaa/faultmanagement/CreateHistoricalAlarmViewTest.java
+++ src/test/java/com/oss/iaa/faultmanagement/CreateHistoricalAlarmViewTest.java
@@ -1,8 +1,10 @@
 package com.oss.iaa.faultmanagement;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import com.oss.BaseTestCase;
+import com.oss.framework.utils.DelayUtils;
+import com.oss.pages.iaa.faultmanagement.FMCreateWAMVPage;
+import com.oss.pages.iaa.faultmanagement.FMSMDashboardPage;
+import io.qameta.allure.Description;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -11,12 +13,8 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.oss.BaseTestCase;
-import com.oss.framework.utils.DelayUtils;
-import com.oss.pages.iaa.faultmanagement.FMCreateWAMVPage;
-import com.oss.pages.iaa.faultmanagement.FMSMDashboardPage;
-
-import io.qameta.allure.Description;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CreateHistoricalAlarmViewTest extends BaseTestCase {
 
@@ -37,13 +35,14 @@ public class CreateHistoricalAlarmViewTest extends BaseTestCase {
         fmsmDashboardPage = FMSMDashboardPage.goToPage(driver, BASIC_URL, FM_DASHBOARD);
     }
 
-    @Parameters({"name", "description", "folderName", "rowNumber"})
+    @Parameters({"name", "description", "folderName", "bookmarkCategory", "rowNumber"})
     @Test(priority = 1, testName = "Create new historical alarm view", description = "Create new historical alarm view")
     @Description("I verify if it is possible to create historical alarm view")
     public void createNewWAMVandDeleteIt(
             @Optional("Selenium_test_alarm_list") String name,
             @Optional("Selenium test description") String description,
             @Optional("Selenium_test_folder") String folderName,
+            @Optional("Selenium_fm_folder") String bookmarkCategory,
             @Optional("0") int rowNumber
 
     ) {
@@ -51,11 +50,12 @@ public class CreateHistoricalAlarmViewTest extends BaseTestCase {
             fmWAMVPage = fmsmDashboardPage.clickCreateNewAlarmList(HISTORICAL_ALARM_MANAGEMENT_VIEW_ID);
             fmWAMVPage.setName(name + FORMATTED_DATE);
             fmWAMVPage.setDescription(description);
+            fmWAMVPage.chooseBookmarkCategory(bookmarkCategory);
             fmWAMVPage.clickNextButton();
             fmWAMVPage.dragAndDropFilterByName(folderName);
+            DelayUtils.sleep(1000);  //  TODO change it after fix OSSNGSA-11102
             fmWAMVPage.selectFilterFromList(rowNumber);
             fmWAMVPage.clickAcceptButton();
-            DelayUtils.sleep(10000);  //  TODO change it after fix OSSNGSA-11102
             Assert.assertTrue(fmsmDashboardPage.checkVisibility(HISTORICAL_ALARM_MANAGEMENT_VIEW_ID, name + FORMATTED_DATE));
         } catch (Exception e) {
             log.error(e.getMessage());
